da179dfed1ba044486cd9c1adfbb41224c83d911
palmtoy <zgli@corp.netease.com>
2013-09-03 09:47:29 +0800

    setTimeout,nextTick,setImmediate

--- Nodejs/NextTickTest/app.js
+++ Nodejs/NextTickTest/app.js
@@ -4,21 +4,34 @@ var forFunc1 = function() {
   for(var i = 0; i < 3; i++) {
     console.log('%j ~ I am in forFunc1 : %s', Date.now(), 'a-'+cnt4Func1);
   }
-  // process.nextTick(forFunc1);
   setTimeout(forFunc1, 0);
   // forFunc1();
 };
 
+/*
 var cnt4Func2 = 0;
 var forFunc2 = function() {
   cnt4Func2++;
   for(var i = 0; i < 3; i++) {
     console.log('%j ~ I am in forFunc2 : %s', Date.now(), 'b-    '+cnt4Func2);
   }
-  process.nextTick(forFunc2);
-  // setTimeout(forFunc2, 0);
+  setImmediate(forFunc2); // node v0.10.x
   // forFunc2();
 };
+*/
+
+var cnt4Func3 = 0;
+var forFunc3 = function() {
+  cnt4Func3++;
+  for(var i = 0; i < 3; i++) {
+    console.log('%j ~ I am in forFunc3 : %s', Date.now(), 'c-       '+cnt4Func3);
+  }
+  process.nextTick(forFunc3); // node v0.8.x
+  // forFunc3();
+};
+
 
 forFunc1();
-forFunc2();
+// forFunc2();
+forFunc3();
+
