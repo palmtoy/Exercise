068589faf18761a6f50652d0cd2033e6a355b4bd
palmtoy <zgli@corp.netease.com>
2013-11-11 15:00:04 +0800

    * without async module

--- Nodejs/ZKTest/createZKNode.js
+++ Nodejs/ZKTest/createZKNode.js
@@ -1,5 +1,4 @@
 var zookeeper = require('node-zookeeper-client');
-var async = require('async');
 var path = require('path')
 
 var client = zookeeper.createClient('localhost:2181');
@@ -14,27 +13,23 @@ if(tmpL.length !== 3 || tmpL[0].length > 0 || tmpL[1].length === 0 || tmpL[2].le
 client.once('connected', function(){
   console.log('Connected to the server.');
 
-  async.waterfall([
-    function(cb){
-      client.create(path.dirname(zkPath), function(err4create){
-        console.log('1 ~ err4create = ', err4create);
-        cb(err4create);
-      });
-    },
-    function(cb){
-      client.create(zkPath, function(err4create){
-        console.log('2 ~ err4create = ', err4create);
-        cb(err4create);
-      });
-    },
-  ], function(err){
-    if(err){
-      console.log('Failed to create node: %s. Due to: %s.', zkPath, err);
+  var firstNode = path.dirname(zkPath);
+  client.create(firstNode, function(err_1){
+    if(err_1){
+      console.log('Failed to create node: %s. Due to: %s.', firstNode, err_1);
     } else{
-      console.log('Node: %s is successfully created.', zkPath);
+      console.log('Node: %s is created successfully.', firstNode);
     }
 
-    client.close();
+    client.create(zkPath, function(err_2){
+      if(err_2){
+        console.log('Failed to create node: %s. Due to: %s.', zkPath, err_2);
+      } else{
+        console.log('Node: %s is created successfully.', zkPath);
+      }
+      client.close();
+    });
+
   });
 
 });
