bf3a2a4b6f38d6ea04cbee5bc5e7b0a360eb7a89
palmtoy <zgli@corp.netease.com>
2013-11-28 17:23:17 +0800

    * add another func

--- Nodejs/JSONStreamTest/app.js
+++ Nodejs/JSONStreamTest/app.js
@@ -2,10 +2,24 @@ var request = require('request')
   , JSONStream = require('JSONStream')
   , es = require('event-stream')
 
+
+/*
 request('http://pomelo3.server.163.org:8080/data.json')
 .pipe(JSONStream.parse('rows.*'))
 .pipe(es.mapSync(function (data) {
   console.log(data)
   return data
 }))
+*/
 
+
+var parser = JSONStream.parse(['rows', true])
+  , req = request({url: 'http://pomelo3.server.163.org:8080/data.json'})
+  , logger = es.mapSync(function (data) {
+      console.error(data)
+      return data
+    });
+
+req
+.pipe(parser)
+.pipe(logger);
