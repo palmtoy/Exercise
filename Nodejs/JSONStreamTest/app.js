24f4175bc328769f75bb2f1a19d28be777c52121
palmtoy <zgli@corp.netease.com>
2013-11-28 17:06:09 +0800

    + add JSONStreamTest dir

---
+++ Nodejs/JSONStreamTest/app.js
@@ -0,0 +1,11 @@
+var request = require('request')
+  , JSONStream = require('JSONStream')
+  , es = require('event-stream')
+
+  request('http://pomelo3.server.163.org:8080/data.json')
+  .pipe(JSONStream.parse('rows.*'))
+  .pipe(es.mapSync(function (data) {
+    console.error(data)
+    return data
+  }))
+
