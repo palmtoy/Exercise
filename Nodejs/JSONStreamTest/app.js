1762fce677bc6b27eb1d1de68f697256c6dfce14
palmtoy <zgli@corp.netease.com>
2013-11-28 17:13:25 +0800

    * modify code format

--- Nodejs/JSONStreamTest/app.js
+++ Nodejs/JSONStreamTest/app.js
@@ -2,10 +2,10 @@ var request = require('request')
   , JSONStream = require('JSONStream')
   , es = require('event-stream')
 
-  request('http://pomelo3.server.163.org:8080/data.json')
-  .pipe(JSONStream.parse('rows.*'))
-  .pipe(es.mapSync(function (data) {
-    console.error(data)
-    return data
-  }))
+request('http://pomelo3.server.163.org:8080/data.json')
+.pipe(JSONStream.parse('rows.*'))
+.pipe(es.mapSync(function (data) {
+  console.log(data)
+  return data
+}))
 
