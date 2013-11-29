07d4d38ccb9fad30613fbfed5f7aed761c6a4914
palmtoy <zgli@corp.netease.com>
2013-11-29 10:36:31 +0800

    * fs, JSONStream.parse doc

--- Nodejs/JSONStreamTest/app.js
+++ Nodejs/JSONStreamTest/app.js
@@ -2,10 +2,19 @@ var fs = require('fs')
   , JSONStream = require('JSONStream');
 
 var stream = fs.createReadStream('data.json', {encoding: 'utf8'});
-parser = JSONStream.parse();
+var parser = JSONStream.parse(['rows', true, 'doc']);
 
 stream.pipe(parser);
 
-parser.on('root', function (obj) {
-  console.log(obj); // whatever you will do with each JSON object
+parser.on('data', function (obj) {
+  console.log('parser.on:data is running ...');
+  console.log(obj);
+  console.log('\n');
 });
+
+parser.on('root', function (obj) {
+  console.log('parser.on:root is running ...');
+  console.log(obj); // whatever you will do with each JSON object
+  console.log('\n');
+});
+
