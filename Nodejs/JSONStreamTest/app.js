401f222cfaa2dd6f0002ea83733cbac1eff7efab
palmtoy <zgli@corp.netease.com>
2013-11-29 13:53:39 +0800

    * use JSONStream.stringify

--- Nodejs/JSONStreamTest/app.js
+++ Nodejs/JSONStreamTest/app.js
@@ -1,22 +1,39 @@
-var fs = require('fs');
-var s = fs.createReadStream('data.json');
-var JSONStream = require('JSONStream');
+var fs = require('fs')
+  , JSONStream = require('JSONStream');
+
+var fsStream = fs.createReadStream('./data.json', {encoding: 'utf8'});
+// var jStream = JSONStream.parse(['rows', true, 'doc'])
+var jStream = JSONStream.parse(['rows', true])
 
 var stringify = JSONStream.stringify();
 stringify.pipe(process.stdout);
 
-var parser = JSONStream.parse([ true ]);
 
-parser.on('data', function (row) {
-  console.log([ row.name, row.relevance ]);
-  if (row._typeGroup === 'entities') {
-    // stringify.write([ row.name, row.relevance ]);
-    console.log([ row.name, row.relevance ]);
+fsStream.pipe(jStream);
+
+jStream.on('data', function(data) {
+  console.log('jStream.on:data is running ...');
+  // console.log('received:', data);
+  stringify.write([ data.doc ]);
+  console.log('\n');
+  /*
+  if (data._typeGroup === 'entities') {
+    stringify.write([ data.name, data.relevance ]);
+  }
+  */
+});
+
+jStream.on('root', function(root, count) {
+  console.log('jStream.on:root is running ...');
+  if (!count) {
+    console.log('no matches found:', root);
+  } else {
+    // console.log('root = ', root); // whatever you will do with each JSON object
+    console.log('count = ', count, '\n');
   }
 });
 
-parser.on('end', function () {
-  // stringify.end();
+jStream.on('end', function () {
+  stringify.end();
 });
 
-s.pipe(parser);
