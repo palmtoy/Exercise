a29430881b0983c4d912d5ba1989744cdb01b9d8
palmtoy <zgli@corp.netease.com>
2013-11-29 10:53:43 +0800

    * use on event

--- Nodejs/JSONStreamTest/app.js
+++ Nodejs/JSONStreamTest/app.js
@@ -1,20 +1,25 @@
 var fs = require('fs')
   , JSONStream = require('JSONStream');
 
-var stream = fs.createReadStream('data.json', {encoding: 'utf8'});
-var parser = JSONStream.parse(['rows', true, 'doc']);
+var fsStream = fs.createReadStream('data.json', {encoding: 'utf8'});
+var stream = JSONStream.parse(['rows', true, 'doc']) //rows, ANYTHING, doc
 
-stream.pipe(parser);
+fsStream.pipe(stream);
 
-parser.on('data', function (obj) {
-  console.log('parser.on:data is running ...');
-  console.log(obj);
+stream.on('data', function(data) {
+  console.log('stream.on:data is running ...');
+  console.log('received:', data);
   console.log('\n');
 });
 
-parser.on('root', function (obj) {
-  console.log('parser.on:root is running ...');
-  console.log(obj); // whatever you will do with each JSON object
-  console.log('\n');
+stream.on('root', function(root, count) {
+  console.log('stream.on:root is running ...');
+  if (!count) {
+    console.log('no matches found:', root);
+  } else {
+    console.log('root = ', root); // whatever you will do with each JSON object
+    console.log('count = ', count);
+    console.log('\n');
+  }
 });
 
