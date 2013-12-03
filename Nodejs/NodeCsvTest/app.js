ba06f9a2607f9c08b3c82319b135e218eda94115
palmtoy <zgli@corp.netease.com>
2013-12-03 16:34:41 +0800

    * print some msg

--- Nodejs/NodeCsvTest/app.js
+++ Nodejs/NodeCsvTest/app.js
@@ -3,16 +3,20 @@
 var fs = require('fs');
 var csv = require('csv');
 
+console.log('__dirname = ', __dirname, '\n');
+
 csv()
 .from.path(__dirname + '/sample.in', { delimiter: ',', escape: '"' })
 .to.stream(fs.createWriteStream(__dirname+'/sample.out'))
 .on('record', function(row, index){
+  console.log('typeof row = ', typeof row);
+  console.log('row.length = ', row.length);
   console.log('#' + index + ' ' + JSON.stringify(row));
 })
 .on('close', function(count){
   // when writing to a file, use the 'close' event
   // the 'end' event may fire before the file has been written
-  console.log('Number of lines: '+count);
+  console.log('\nNumber of lines: '+count);
 })
 .on('error', function(err){
   console.error(err.message);
