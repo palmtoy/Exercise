e7afc499103e7c96d12331b449849ff80ae3c699
palmtoy <zgli@corp.netease.com>
2013-12-06 09:41:10 +0800

    * test realpath

--- Nodejs/FsTest/app.js
+++ Nodejs/FsTest/app.js
@@ -1,7 +1,20 @@
 var fs = require('fs');
 
-var cache = {'/etc':'/private/etc'};
+var cache = {'/etc': '/private/etc'};
+// var cache = {'/etc':'/etc'};
+// var cache = {};
 fs.realpath('/etc/passwd', cache, function (err, resolvedPath) {
   if (err) throw err;
   console.log(resolvedPath);
+  console.log('cache = ', cache);
 });
+
+/*
+var cache = {'/temp': './Backup/temp/app10.js', '/temp2': './Backup/temp'};
+fs.realpath('/temp', cache, function (err, resolvedPath) {
+  if (err) throw err;
+  console.log(resolvedPath);
+  console.log('cache = ', cache);
+});
+*/
+
