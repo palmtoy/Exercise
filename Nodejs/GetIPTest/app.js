a97cedd0f48d86d065c7bff0e45b1db07fbd862e
palmtoy <zgli@corp.netease.com>
2013-12-10 10:56:58 +0800

    * optimize code

--- Nodejs/GetIPTest/app.js
+++ Nodejs/GetIPTest/app.js
@@ -3,9 +3,10 @@ var IPv4, hostName;
 
 hostName = os.hostname();
 
-for(var i = 0; i < os.networkInterfaces().en0.length; i++){
-  if(os.networkInterfaces().en0[i].family === 'IPv4'){
-    IPv4 = os.networkInterfaces().en0[i].address;
+var tmpI = os.networkInterfaces().en0;
+for(var i = 0; i < tmpI.length; i++){
+  if(tmpI[i].family === 'IPv4'){
+    IPv4 = tmpI[i].address;
   }
 }
 
