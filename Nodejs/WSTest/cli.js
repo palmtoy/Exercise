808c6c78c9ef2d1076cb2984bd8ef3f03a44c5f8
palmtoy <zgli@corp.netease.com>
2013-12-19 17:28:40 +0800

    * read binary data from buffer

--- Nodejs/WSTest/cli.js
+++ Nodejs/WSTest/cli.js
@@ -7,23 +7,43 @@ var intervalId = null;
 
 ws.on('open', function() {
   console.log('Connected to server.');
+  var headLen = 4
+    , itemLen = 4;
 
   function sendArray() {
-    var array = new Float32Array(3);
-    for (var i = 0; i < array.length; ++i) array[i] = Math.round(Math.random() * 0xFF) / 9;
+    var len = Math.floor((Math.random() * 5) + 1);
+
+    var head = new Int32Array([len]);
+
+    var body = new Float32Array(len);
+    for (var i = 0; i < body.length; ++i) {
+      body[i] = Math.round(Math.random() * 0xFF) / 3;
+    }
+    console.log('body = %j', body);
+
+    var buf = new Buffer(headLen + itemLen * len);
+    buf.writeInt32BE(head[0], 0);
+
+    var offset = 0;
+    for(var i = 0; i < len; ++i) {
+      buf.writeFloatBE(body[i], headLen + offset * itemLen);
+      ++offset;
+    }
+
     try {
-      ws.send(array, {binary: true, mask: false});
+      ws.send(buf, {binary: true, mask: false});
     } catch(err) {
       throw err;
     }
-    console.log('Send %j to svr ...', array);
+
+    console.log('Send %j to svr ...', buf);
   }
 
   intervalId = setInterval(sendArray, 1500);
 });
 
 ws.on('message', function(message) {
-  console.log('\nEcho from svr: %j', message);
+  console.log('\nEcho from svr: %s', message);
   console.log('====================================\n');
 });
 
--- Nodejs/WSTest/svr.js
+++ Nodejs/WSTest/svr.js
@@ -4,23 +4,32 @@ var WebSocketServer = require('ws').Server
   , port = 8086
   , wss = new WebSocketServer({port: port});
 
-var FileReader = require('FileReader');
-
 var msgId = 0;
 wss.on('connection', function(ws) {
+  var headLen = 4
+    , itemLen = 4;
+
   ws.on('message', function(buf) {
     console.log('\nisBuffer(buf) = ', Buffer.isBuffer(buf));
     console.log('Received from cli: %d ~ %j', ++msgId, buf);
 
-    var num1 = buf.readFloatLE(0);
-    console.log('num1 = ', num1);
-    var num2 = buf.readFloatLE(4);
-    console.log('num2 = ', num2);
-    var num3 = buf.readFloatLE(8);
-    console.log('num3 = ', num3);
+    var len = buf.readInt32BE(0);
+    console.log('len = ', len);
+    var offset = 0;
+    var body = [];
+    for(var i = 0; i < len; ++i) {
+      body.push(buf.readFloatBE(headLen + offset * itemLen));
+      ++offset;
+    }
+    console.log('body = %j', body);
 
-    // ws.send(msgId + ' ~ ' + buf, {binary: true, mask: false});
-    ws.send(buf, {binary: true, mask: false});
+    var sum = 0;
+    for(var k in body) {
+      sum += body[k];
+    }
+    console.log('sum = ', sum);
+
+    ws.send(msgId + ' ~ ' + sum.toString());
   });
 });
 
