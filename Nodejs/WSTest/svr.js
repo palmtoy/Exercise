de2ed67619fc7ebde0a4de473a3d244ec6b54909
palmtoy <zgli@corp.netease.com>
2013-12-19 16:14:42 +0800

    * add binary type data transport

--- Nodejs/WSTest/svr.js
+++ Nodejs/WSTest/svr.js
@@ -8,10 +8,19 @@ var FileReader = require('FileReader');
 
 var msgId = 0;
 wss.on('connection', function(ws) {
-  ws.on('message', function(msg) {
-    console.log('Received from cli: %d ~ %j', ++msgId, msg);
-    // ws.send(msgId + ' ~ ' + msg, {binary: true, mask: false});
-    ws.send(msg, {binary: true, mask: false});
+  ws.on('message', function(buf) {
+    console.log('\nisBuffer(buf) = ', Buffer.isBuffer(buf));
+    console.log('Received from cli: %d ~ %j', ++msgId, buf);
+
+    var num1 = buf.readFloatLE(0);
+    console.log('num1 = ', num1);
+    var num2 = buf.readFloatLE(4);
+    console.log('num2 = ', num2);
+    var num3 = buf.readFloatLE(8);
+    console.log('num3 = ', num3);
+
+    // ws.send(msgId + ' ~ ' + buf, {binary: true, mask: false});
+    ws.send(buf, {binary: true, mask: false});
   });
 });
 
