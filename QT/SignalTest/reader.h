33b1e13c627b61688a7f86ecf76beb546ff7d12c
palmtoy <zgli@corp.netease.com>
2014-04-27 17:43:36 +0800

    + add new dir SignalTest

---
+++ QT/SignalTest/reader.h
@@ -0,0 +1,15 @@
+#include <QObject>
+#include <QDebug>
+
+class Reader : public QObject
+{
+  Q_OBJECT
+  public:
+    Reader() {}
+
+    public slots:
+      void receiveNewspaper(const QString & name) const
+      {
+        qDebug() << "Receives Newspaper: " << name;
+      }
+};
