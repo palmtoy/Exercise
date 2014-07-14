bfff9ae78c335a7c147574902e23a5d862c3433a
palmtoy <zgli@kabaminc.com>
2014-07-14 13:37:34 +0800

    * test with chrome

--- Nodejs/SupertestTest/app.js
+++ Nodejs/SupertestTest/app.js
@@ -1,20 +1,31 @@
+/*
+
+chrome ~ http://localhost:8081/user
+
+*/
+
 var request = require('supertest')
   , express = require('express');
+
+var port = 8081;
 
 var app = express();
 
 app.get('/user', function(req, res){
-  res.send(200, { name: 'tobi' });
+  res.send(200, { name: 'palmtoy' });
 });
+
+app.listen(port);
+console.log('Express http server is running on', port, '...');
 
 request(app)
 .get('/user')
 .expect('Content-Type', /json/)
-.expect('Content-Length', '15')
+.expect('Content-Length', '18')
 .expect(200)
 .end(function(err, res){
   if (err) throw err;
   // console.log('res = ', res);
-  console.log('res.body = ', res.body); // res.body =  { name: 'tobi' }
+  console.log('res.body = ', res.body); // res.body =  { name: 'palmtoy' }
 });
 
