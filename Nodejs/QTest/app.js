3201a279cc2bc91ba8835864f29746ae053efa2c
Zhigang Li <zgli@kabaminc.com>
2014-09-02 16:09:46 +0800

    Jira: GAM-0	Description: test google/baidu	Reviewer: none

--- Nodejs/QTest/app.js
+++ Nodejs/QTest/app.js
@@ -5,13 +5,20 @@ var FS = require('fs'),
 function getResults(pathToFile) {   
     return Q.nfcall(FS.readFile, pathToFile, "utf-8")
     .then(function(tmpPrefix) {
-        console.log('tmpPrefix = ', tmpPrefix);
+        tmpPrefix = tmpPrefix.slice(0, -1);
         var options = { headers: {'User-Agent': 'MyAgent'} }; // github requires user agent string
-        return [Q.nfcall(request, 'http://www.google.com', options),
-                Q.nfcall(request, 'http://www.baidu.com', options)];
+
+        var urlG = 'https://' + tmpPrefix + '.google.com/';
+        var urlB = 'http://' + tmpPrefix + '.baidu.com';
+
+        console.log('urlG = ', urlG);
+        console.log('urlB = ', urlB, '\n\n');
+
+        return [Q.nfcall(request, urlG, options),
+                Q.nfcall(request, urlB, options)];
     })
-    .spread(function(collaboratorsRes, commitsRes) {        
-        return [collaboratorsRes[1], commitsRes[1]];  // return the response body
+    .spread(function(googleRet, baiduRet) {
+        return [googleRet[1], baiduRet[1]];  // return the response body
     })
     .fail(function(err) {
         console.error(err)
@@ -24,8 +31,8 @@ function getResults(pathToFile) {
 getResults('w.txt').then(function(responses) {
     // do something with the responses
   console.log('responses[0] = ', responses[0]);
-  console.log('\n ************************************************* ');
+  console.log('\n\n ************************************************* ');
   console.log(' ************************************************* ');
-  console.log(' ************************************************* \n');
+  console.log(' ************************************************* \n\n');
   console.log('responses[1] = ', responses[1]);
 });
