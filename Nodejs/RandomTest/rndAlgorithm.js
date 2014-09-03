0a96eac78e09ec0fcfb88dd83a5c2b0340ffea52
Zhigang Li <zgli@kabaminc.com>
2014-09-03 10:41:16 +0800

    Jira: GAM-0	Description: add new file rndAlgorithm	Reviewer: none

--- Nodejs/RandomTest/rndAlgorithm.js
+++ Nodejs/RandomTest/rndAlgorithm.js
@@ -1,12 +1,30 @@
-function getRandomBytes (howMany) {
-  var fs= require('fs');
-  var bytes= new Buffer(howMany);
+var fs= require('fs');
+
+var times = process.argv[2];
+var numLen = 32;
+var howMany = numLen * times;
+
+console.log('howMany = ', howMany, '\n');
+
+function getRandomBytes () {
+  var buf= new Buffer(howMany);
   var fd= fs.openSync('/dev/random', 'r');
-  fs.readSync(fd, bytes, 0, howMany, null);
+  fs.readSync(fd, buf, 0, howMany, null);
   fs.closeSync(fd)
-  //return bytes;
-  return bytes.toString();
+
+  console.log('buf.length = ', buf.length, '\n');
+
+  var retArr = [];
+  for(var i = 0; i < times; i++) {
+    var offset = i * numLen;
+    var v = buf.readInt32LE(offset);
+    v = v > 0 ? v : -v;
+    retArr.push(v);
+  }
+
+  console.log('retArr.length = %j \n', retArr.length);
+  console.log('retArr = %j', retArr);
 }
 
-console.log(getRandomBytes(48));
+getRandomBytes();
 
