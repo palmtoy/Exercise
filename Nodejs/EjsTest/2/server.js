193610a82130bebb2c5c1f1aaae700715dc19ff2
Zhigang Li <zgli@kabaminc.com>
2014-09-08 15:50:16 +0800

    + add EjsTest folder

---
+++ Nodejs/EjsTest/2/server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+
+// Register ejs as .html. If we did
+// not call this, we would need to
+// name our views foo.ejs instead
+// of foo.html. The __express method
+// is simply a function that engines
+// use to hook into the Express view
+// system by default, so if we want
+// to change "foo.ejs" to "foo.html"
+// we simply pass _any_ function, in this
+// case `ejs.__express`.
+app.engine('.html', require('ejs').__express);
+
+// Optional since express defaults to CWD/views
+app.set('views', __dirname + '/views');
+
+// Without this you would need to
+// supply the extension to res.render()
+// ex: res.render('users.html').
+app.set('view engine', 'html');
+
+// Dummy users
+var users = [
+  { name: 'Will Lee', email: 'palmtoy@gmail.com' },
+  { name: 'tobi', email: 'tobi@learnboost.com' },
+  { name: 'loki', email: 'loki@learnboost.com' },
+  { name: 'jane', email: 'jane@learnboost.com' }
+];
+
+app.get('/', function(req, res){
+  res.render('users', {
+    users: users,
+    title: "EJS example",
+    header: "Some users"
+  });
+});
+
+
+var port = 8089;
+app.listen(port);
+
+console.log('Express app started on port %d ...', port);
+
