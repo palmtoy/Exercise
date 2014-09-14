124cef09cad8be5a1308005ce2d4f411f17789a0
palmtoy <zgli@corp.netease.com>
2014-09-14 20:10:26 +0800

    * add a test file of NodeInspector

--- Nodejs/NodeInspector/app-1.js
+++ Nodejs/NodeInspector/app-1.js
@@ -5,15 +5,11 @@ $ node --debug-brk app.js
 http://127.0.0.1:8080/debug?port=5858
 */
 
-var foo = function(n) {
-  var sum = 0
-  for(var i = 1; i <= n; i++) {
-    sum += i;
-  }
-
+var funcAdd = function(paramA, paramB) {
+  var sum = paramA + paramB;
   return sum;
 }
 
-var ret = foo(100);
+var ret = funcAdd(1, 2);
 console.log('ret =', ret);
 
