9ca9bc8b0c8e176105d025412630bd5b595a281d
Zhigang Li <zgli@kabaminc.com>
2014-09-18 14:29:19 +0800

    + add new folder RefConstTest

---
+++ Nodejs/RefConstTest/app.js
@@ -0,0 +1,4 @@
+var consts = require('./consts');
+
+console.log('consts.REF_CLA.REF_WARRIOR = ', consts.REF_CLA.REF_WARRIOR);
+
---
+++ Nodejs/RefConstTest/consts.js
@@ -0,0 +1,12 @@
+module.exports = {
+  CLASS_ID: {
+    WARRIOR: 'Warrior',
+    ARCHER: 'Archer',
+    MAGE: 'Mage'
+  }
+};
+  
+module.exports.REF_CLA = {
+  REF_WARRIOR: module.exports.CLASS_ID.WARRIOR
+}
+
