318fcce74d612f4d2877af10597669bae5581fce
Zhigang Li <zgli@kabaminc.com>
2014-09-19 15:29:51 +0800

    * polish the code of cli.js

--- Nodejs/ConnectTest/cli.js
+++ Nodejs/ConnectTest/cli.js
@@ -16,9 +16,20 @@ request('http://127.0.0.1:3000/hi', function (error, response, body) {
 
 
 request.post({
-  headers: {'content-type' : 'application/x-www-form-urlencoded'},
+  headers: {'Content-type': 'application/x-www-form-urlencoded', charset: 'utf-8'},
   url:     'http://localhost:3000/wow',
-  body:    "user=will"
+  body:    'user=will'
+}, function(error, response, body){
+  if (!error && response.statusCode == 200) {
+    console.log(body);
+  }
+});
+
+
+request.post({
+  headers: {'Content-type': 'application/x-www-form-urlencoded', charset: 'utf-8'},
+  url:     'http://localhost:3000/qs',
+  body:    'user[name][first]=will&user[email]=zgli@kabaminc.com'
 }, function(error, response, body){
   if (!error && response.statusCode == 200) {
     console.log(body);
