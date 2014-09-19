107c71a2fa9612706db2cc3c37fbecca4fffa9ee
Zhigang Li <zgli@kabaminc.com>
2014-09-19 15:37:20 +0800

    + add new folder CombatMsgTest

---
+++ Nodejs/CombatMsgTest/cli.js
@@ -0,0 +1,38 @@
+var request = require('request');
+
+
+request('http://127.0.0.1:3000', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body);
+  }
+});
+
+
+request('http://127.0.0.1:3000/hi', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body);
+  }
+});
+
+
+request.post({
+  headers: {'Content-type': 'application/x-www-form-urlencoded', charset: 'utf-8'},
+  url:     'http://localhost:3000/wow',
+  body:    'user=will'
+}, function(error, response, body){
+  if (!error && response.statusCode == 200) {
+    console.log(body);
+  }
+});
+
+
+request.post({
+  headers: {'Content-type': 'application/x-www-form-urlencoded', charset: 'utf-8'},
+  url:     'http://localhost:3000/qs',
+  body:    'user[name][first]=will&user[email]=zgli@kabaminc.com'
+}, function(error, response, body){
+  if (!error && response.statusCode == 200) {
+    console.log(body);
+  }
+});
+
