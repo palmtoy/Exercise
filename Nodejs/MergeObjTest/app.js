d5bd6dd23e61b8fb14bf53d3fdc0204ca951932b
Zhigang Li <zgli@kabaminc.com>
2014-10-15 14:43:24 +0800

    * use utils.js

--- Nodejs/MergeObjTest/app.js
+++ Nodejs/MergeObjTest/app.js
@@ -23,44 +23,10 @@ var targetObj = {
 };
 
 
-function deepExtend(desObj, srcObj) {
-  var doExtend = function(k) {
-    // Property in destination object set; update its value.
-    if(typeof srcObj[k] === "object" && srcObj[k] !== null) {
-      desObj[k] = deepExtend(desObj[k], srcObj[k]);
-    } else {
-      desObj[k] = srcObj[k];
-    }
-  };
-
-  if(typeof srcObj !== "object" || srcObj === null) {
-    return desObj;
-  }
-
-  if(srcObj instanceof Array) {
-    desObj = desObj || [];
-    if(desObj instanceof Array) {
-      for(var i = 0; i < srcObj.length; i++) {
-        doExtend(i);
-      }
-    }
-  } else {
-    desObj = desObj || {};
-    if(desObj instanceof Object) {
-      for (var p in srcObj) {
-        if (srcObj.hasOwnProperty(p)) {
-          doExtend(p);
-        }
-      }
-    }
-  }
-
-  return desObj;
-};
-
+var utils = require('./utils');
 
 var tmpObj = {};
-tmpObj = deepExtend(tmpObj, targetObj);
+tmpObj = utils.deepExtend(tmpObj, targetObj);
 tmpObj.inventory['11476'].inventory_id = 20000;
 tmpObj.gemList[0].blue[0].PATK = 9;
 
