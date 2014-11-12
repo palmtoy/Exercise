2578187fccfd636a06d7a38ef7e2a2ef68833d7a
Zhigang Li <zgli@kabaminc.com>
2014-11-12 17:42:48 +0800

    * set state directly

---
+++ Nodejs/StateMachineTest/Backup/app-8.js
@@ -0,0 +1,20 @@
+var jsm = require("javascript-state-machine");
+
+var fsm = jsm.StateMachine.create({
+  events: [
+    { name: 'startup', from: 'none',  to: 'green' },
+    { name: 'panic',   from: 'green', to: 'red'   },
+    { name: 'calm',    from: 'red',   to: 'green' },
+  ]});
+
+console.log('1 ~ current state:', fsm.current); // "none"
+
+fsm.startup();
+
+console.log('2 ~ current state:', fsm.current); // "green"
+
+fsm.current = 'red';
+fsm.calm();
+
+console.log('3 ~ current state:', fsm.current); // "green"
+
