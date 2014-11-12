8975733863b0b12325580f9ad64506f222f4859c
Zhigang Li <zgli@kabaminc.com>
2014-11-12 20:00:44 +0800

    + add TLSTest folder

---
+++ Nodejs/HttpTest/HttpsSvr/TLSTest/app.js
@@ -0,0 +1,26 @@
+var tls = require('tls');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('test/fixtures/keys/server-key.pem'),
+  cert: fs.readFileSync('test/fixtures/keys/server-cert.pem'),
+
+  // This is necessary only if using the client certificate authentication.
+  requestCert: true,
+
+  // This is necessary only if the client uses the self-signed certificate.
+  ca: [ fs.readFileSync('test/fixtures/keys/client-cert.pem') ]
+};
+
+var server = tls.createServer(options, function(cleartextStream) {
+  console.log('server connected',
+              cleartextStream.authorized ? 'authorized' : 'unauthorized');
+  cleartextStream.write("welcome!\n");
+  cleartextStream.setEncoding('utf8');
+  cleartextStream.pipe(cleartextStream);
+});
+
+server.listen(8000, function() {
+  console.log('server bound');
+});
+
