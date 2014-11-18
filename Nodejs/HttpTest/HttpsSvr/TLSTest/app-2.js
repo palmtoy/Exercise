2f5d6edd6a3607c8af6e515951dd3a9de039c5aa
Zhigang Li <zgli@kabaminc.com>
2014-11-18 20:08:29 +0800

    + add some testing files

--- Nodejs/HttpTest/HttpsSvr/TLSTest/app-2.js
+++ Nodejs/HttpTest/HttpsSvr/TLSTest/app-2.js
@@ -4,19 +4,28 @@ var tls = require('tls');
 var fs = require('fs');
 
 var options = {
-  pfx: fs.readFileSync('../test/fixtures/keys/server.pfx'),
+  key: fs.readFileSync('../test/fixtures/keys/server-key.pem'),
+  cert: fs.readFileSync('../test/fixtures/keys/server-cert.pem'),
 
   // This is necessary only if using the client certificate authentication.
   requestCert: true,
+
+  // This is necessary only if the client uses the self-signed certificate.
+  ca: [ fs.readFileSync('../test/fixtures/keys/client-cert.pem') ]
 };
 
+console.log('options.ca = ', options.ca.toString());
+
 var server = tls.createServer(options, function(cleartextStream) {
+  console.log('cleartextStream = ', cleartextStream);
+
   console.log('server connected',
               cleartextStream.authorized ? 'authorized' : 'unauthorized');
-  cleartextStream.write("Welcome!\n");
+  cleartextStream.write("\nwelcome!\n");
   cleartextStream.setEncoding('utf8');
   cleartextStream.pipe(cleartextStream);
 });
+
 server.listen(8000, function() {
   console.log('server bound');
 });
