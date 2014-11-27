0a167972e13caede3ca68c18fa3f408fec666914
Zhigang Li <zgli@kabaminc.com>
2014-11-27 11:23:29 +0800

    + add cluster test folder

---
+++ Nodejs/ClusterTest/cli.js
@@ -0,0 +1,26 @@
+/*
+node cli.js 
+node cli.js localhost 8000
+*/
+
+var http = require('http');
+
+// console.log('argv = ', JSON.stringify(process.argv));
+
+var options = {
+  host: process.argv[2] || 'localhost',
+  port: process.argv[3] || 8000,
+  path: '/',
+  method: 'GET'
+};
+
+var req = http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function(chunk) {
+    console.log(chunk);
+  });
+});
+
+req.end();
---
+++ Nodejs/ClusterTest/server.js
@@ -0,0 +1,25 @@
+// NODE_DEBUG=cluster node server.js
+
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case its a HTTP server
+  http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end("hello world\n");
+    console.log(process.pid + ' is servering ...');
+  }).listen(8000);
+}
+
