d1c758b7183f4665c62b71230eb34de9822b0ab1
Zhigang Li <zgli@kabaminc.com>
2014-11-27 18:02:16 +0800

    * add some comments

---
+++ Nodejs/ClusterTest/svr-1.js
@@ -0,0 +1,27 @@
+// NODE_DEBUG=cluster node svr.js
+
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length;
+
+// console.log('\n' + process.pid + ', process.env.NODE_UNIQUE_ID = ' + process.env.NODE_UNIQUE_ID + '\n');
+
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case its a HTTP server
+  http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end("hello world\n");
+    console.log(process.pid + ' is servering ...');
+  }).listen(8000);
+}
+
--- Nodejs/ClusterTest/svr.js
+++ Nodejs/ClusterTest/svr.js
@@ -1,4 +1,4 @@
-// NODE_DEBUG=cluster node server.js
+// NODE_DEBUG=cluster node svr.js
 
 var cluster = require('cluster');
 var http = require('http');
@@ -6,15 +6,31 @@ var numCPUs = require('os').cpus().length;
 
 // console.log('\n' + process.pid + ', process.env.NODE_UNIQUE_ID = ' + process.env.NODE_UNIQUE_ID + '\n');
 
+var timeouts = [];
+function errorMsg() {
+  console.error("Something must be wrong with the connection ...");
+}
+
+
 if (cluster.isMaster) {
   // Fork workers.
   for (var i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
+  cluster.on('fork', function(worker) {
+    timeouts[worker.id] = setTimeout(errorMsg, 2000);
+    console.error(worker.id + " is forked.");
   });
+  cluster.on('listening', function(worker, address) {
+    clearTimeout(timeouts[worker.id]);
+    console.error(worker.id + " is listening.");
+  });
+  cluster.on('exit', function(worker, code, signal) {
+    clearTimeout(timeouts[worker.id]);
+    errorMsg();
+  });
+
 } else {
   // Workers can share any TCP connection
   // In this case its a HTTP server
@@ -24,4 +40,8 @@ if (cluster.isMaster) {
     console.log(process.pid + ' is servering ...');
   }).listen(8000);
 }
+
+
+
+
 
