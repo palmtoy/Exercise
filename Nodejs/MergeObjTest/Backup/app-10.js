8df506ebd9c6d66dc9a60e8eae1353872e3425b6
Zhigang Li <zgli@kabaminc.com>
2014-12-15 18:13:17 +0800

    * modify the folder

---
+++ Nodejs/MergeObjTest/Backup/app-10.js
@@ -0,0 +1,18 @@
+var lodash = require('lodash');
+
+var srcObj = {"items":[{"type":"gaminventory","data":"GEM_PDEF_2","quantity":1,"result":{"uid":1001440,"economy_id":"GEM_PDEF_2","system":"Gem","inventory_id":17452,"num":1,"location":"bag_items","cell":1,"_ts":1417749506,"_id":"54812402292ca90000667b92"}},{"type":"gaminventory","data":"ITM_ENCHANT_LuckStone_2","quantity":1,"result":{"uid":1001440,"economy_id":"ITM_ENCHANT_LuckStone_2","system":"LuckStone","inventory_id":17453,"num":1,"location":"bag_items","cell":1,"_ts":1417749506,"_id":"54812402292ca90000667b94"}},{"type":"gaminventory","data":"GEM_PATK_1","quantity":1,"result":{"uid":1001440,"economy_id":"GEM_PATK_1","system":"Gem","inventory_id":17454,"num":1,"location":"bag_items","cell":1,"_ts":1417749506,"_id":"54812402292ca90000667b95"}}]}; 
+
+console.log('srcObj = ', JSON.stringify(srcObj));
+console.log('\n\n');
+
+var targetObj = {"items":[{"type":"gaminventory","data":"ITM_ENCHANT_LuckStone_3","quantity":1,"result":{"uid":1001440,"economy_id":"ITM_ENCHANT_LuckStone_3","system":"LuckStone","inventory_id":17451,"num":1,"location":"bag_items","cell":1,"_ts":1417749506,"_id":"54812402292ca90000667b91"}}]};
+
+console.log('   targetObj = ', JSON.stringify(targetObj));
+console.log('\n\n');
+
+
+var newObj = lodash.merge(targetObj, srcObj);
+
+console.log('   newObj = ', JSON.stringify(newObj), '\n\n');
+console.log('   targetObj = ', JSON.stringify(targetObj));
+
