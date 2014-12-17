9f084cc129b30d8a13f39d568d4e92f360c1f775
Zhigang Li <zgli@kabaminc.com>
2014-12-17 19:38:37 +0800

    + read line testing files

---
+++ Nodejs/ReadlineTest/Backup/app-2.js
@@ -0,0 +1,29 @@
+function ask(question, format, callback) {
+  var stdin = process.stdin
+    , stdout = process.stdout;
+
+  stdin.resume();
+  stdout.write(question + ": ");
+
+  stdin.once('data', function(data) {
+    data = data.toString().trim();
+
+    if (format.test(data)) {
+      callback(data);
+    } else {
+      stdout.write("It should match: "+ format +"\n");
+      ask(question, format, callback);
+    }
+  });
+}
+
+
+ask("Name ", /.+/, function(name) {
+  ask("Email", /^.+@.+$/, function(email) {
+    console.log("\nYour name  is:", name);
+    console.log("Your email is:", email);
+
+    process.exit();
+  });
+});
+
---
+++ Nodejs/ReadlineTest/Backup/app-4.js
@@ -0,0 +1,34 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function hidden(query, callback) {
+  var stdin = process.openStdin(),
+  i = 0;
+  process.stdin.on("data", function(char) {
+    char = char + "";
+    switch (char) {
+      case "\n":
+        case "\r":
+        case "\u0004":
+        stdin.pause();
+      break;
+      default:
+        process.stdout.write("\033[2K\033[200D"+query+"["+((i%2==1)?"=-":"-=")+"]");
+      i++;
+      break;
+    }
+  });
+
+  rl.question(query, function(value) {
+    rl.history = rl.history.slice(1);
+    callback(value);
+  });
+}
+
+hidden("password: ", function(password) {
+  console.log("Your password: " + password);
+});
