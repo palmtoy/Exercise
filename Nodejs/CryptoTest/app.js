07b0e6164db7ef12c3f50b29a9afa7eb2550310d
Zhigang Li <zgli@kabaminc.com>
2014-12-17 10:40:50 +0800

    * use base64

--- Nodejs/CryptoTest/app.js
+++ Nodejs/CryptoTest/app.js
@@ -1,11 +1,8 @@
-var crypto = require('crypto');
-var content = 'password'
-var md5 = crypto.createHash('md5');
-md5.update(content);
-var d = md5.digest('hex');  //MD5值是5f4dcc3b5aa765d61d8327deb882cf99
-console.log(' md5(%j) = ', content, d);
+var str = 'Hello World';
 
-var shasum = crypto.createHash('sha1');
-shasum.update(content);
-var d = shasum.digest('hex');
-console.log('sha1(%j) = ', content, d);
+var str4base64 = new Buffer(str).toString('base64');
+console.log('\nstr4base64 = ' + str4base64); // SGVsbG8gV29ybGQ=
+
+var str4origin = new Buffer(str4base64, 'base64').toString('ascii');
+console.log('\nstr4origin = ' + str4origin); // Hello World
+
