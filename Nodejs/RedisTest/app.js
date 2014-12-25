d013ce2a3cbc81934dede2ed7806c88238700d15
Zhigang Li <zgli@kabaminc.com>
2014-12-25 11:22:11 +0800

    * ues global_msg_num

--- Nodejs/RedisTest/app.js
+++ Nodejs/RedisTest/app.js
@@ -1,37 +1,35 @@
 var redis = require("redis"),
-client = redis.createClient();
+client = redis.createClient(), multi;
 
 client.select(1, function(err, rep) {
   if(err) {
     return console.log('err = ', err);
   }
 
-  var args = [ 'myzset', 1, 'one', 2, 'two', 3, 'three', 99, 'ninety-nine' ];
-  client.zadd(args, function (err, response) {
-    if (err) throw err;
-    console.log('added '+response+' items.');
+  // start a separate multi command queue
+  multi = client.multi([
+    ["mget", "global_msg_num", 0, redis.print],
+    ["incr", "global_msg_num"],
+    ["incr", "global_msg_num"],
+    ["incr", "global_msg_num"]
+  ]);
 
-    // -Infinity and +Infinity also work
-    // var args1 = [ 'myzset', '+inf', '-inf' ];
-    // client.zrevrangebyscore(args1, function (err, response) {
-    var args1 = [ 'myzset', '-inf', '+inf' ];
-    client.zrangebyscore(args1, function (err, response) {
-      if (err) throw err;
-      console.log('example1', response);
-      // write your code here
-    });
 
-    var max = 3, min = 1, offset = 1, count = 2;
-    var args2 = [ 'myzset', max, min, 'WITHSCORES', 'LIMIT', offset, count ];
-    client.zrevrangebyscore(args2, function (err, response) {
-      if (err) throw err;
-      console.log('example2', response);
-      // write your code here
-    });
+  multi.incr("global_msg_num", redis.print);
+
+  // drains multi queue and runs atomically
+  multi.exec(function (err, replies) {
+    console.log(replies);
+  });
+
+  // you can re-run the same transaction if you like
+  multi.exec(function (err, replies) {
+    console.log(replies);
   });
 
   setTimeout(function() {
     client.end();
   }, 1000);
+
 });
 
