a752cc185898b42060e88c5a6b3df1e4e3fdd8cf
Zhigang Li <zgli@kabaminc.com>
2015-01-05 18:17:32 +0800

    * misc

--- Nodejs/RedisTest/examples/backpressure_drain.js
+++ Nodejs/RedisTest/examples/backpressure_drain.js
@@ -1,33 +1,40 @@
-var redis = require("../index"),
+var redis = require("redis"),
     client = redis.createClient(null, null, {
         command_queue_high_water: 5,
         command_queue_low_water: 1
     }),
-    remaining_ops = 100000, paused = false;
+    remaining_ops = 100, paused = false;
 
-function op() {
-    if (remaining_ops <= 0) {
-        console.error("Finished.");
-        process.exit(0);
-    }
+client.select(1, function(err, rep) {
+  if(err) {
+    return console.log('err = ', err);
+  }
 
-    remaining_ops--;
-    if (client.hset("test hash", "val " + remaining_ops, remaining_ops) === false) {
-        console.log("Pausing at " + remaining_ops);
-        paused = true;
-    } else {
-        process.nextTick(op);
-    }
-}
+  function op() {
+      if (remaining_ops <= 0) {
+          console.error("Finished.");
+          process.exit(0);
+      }
 
-client.on("drain", function () {
-    if (paused) {
-        console.log("Resuming at " + remaining_ops);
-        paused = false;
-        process.nextTick(op);
-    } else {
-        console.log("Got drain while not paused at " + remaining_ops);
-    }
+      remaining_ops--;
+      if (client.hset("test_hash", "val_" + remaining_ops, remaining_ops) === false) {
+          console.log("Pausing at " + remaining_ops);
+          paused = true;
+      } else {
+          process.nextTick(op);
+      }
+  }
+
+  client.on("drain", function () {
+      if (paused) {
+          console.log("Resuming at " + remaining_ops);
+          paused = false;
+          process.nextTick(op);
+      } else {
+          console.log("Got drain while not paused at " + remaining_ops);
+      }
+  });
+
+  op();
 });
 
-op();
