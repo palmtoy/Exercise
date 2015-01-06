e867c1f16cc7fe22bfb094d81c34ff9424b8dbf8
Zhigang Li <zgli@kabaminc.com>
2015-01-06 15:24:33 +0800

    * testing eval.js

--- Nodejs/RedisTest/examples/eval.js
+++ Nodejs/RedisTest/examples/eval.js
@@ -1,14 +1,26 @@
-var redis = require("../index"),
+var redis = require("redis"),
     client = redis.createClient();
 
 redis.debug_mode = true;
 
-client.eval("return 100.5", 0, function (err, res) {
-    console.dir(err);
-    console.dir(res);
+client.select(1, function(err, rep) {
+  if(err) {
+    return console.log('err = ', err);
+  }
+
+  client.eval("return 100.5", 0, function (err, res) {
+      console.dir(err);
+      console.dir(res);
+  });
+
+  client.eval([ "return 100.5", 0 ], function (err, res) {
+      console.dir(err);
+      console.dir(res);
+  });
+
+  setTimeout(function() {
+    client.quit();
+  }, 500);
+
 });
 
-client.eval([ "return 100.5", 0 ], function (err, res) {
-    console.dir(err);
-    console.dir(res);
-});
