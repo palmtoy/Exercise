372311540c6a852e285898506c6517a59a7bb4ca
Zhigang Li <zgli@kabaminc.com>
2015-01-06 19:55:22 +0800

    * subqueries

--- Nodejs/RedisTest/examples/subqueries.js
+++ Nodejs/RedisTest/examples/subqueries.js
@@ -3,13 +3,20 @@
 //
 var client = require("redis").createClient();
 
-client.keys("*", function (err, keys) {
+client.select(1, function(err, rep) {
+  if(err) {
+    return console.log('err = ', err);
+  }
+
+  client.keys("*", function (err, keys) {
     keys.forEach(function (key, pos) {
-        client.type(key, function (err, keytype) {
-            console.log(key + " is " + keytype);
-            if (pos === (keys.length - 1)) {
-                client.quit();
-            }
-        });
+      client.type(key, function (err, keytype) {
+        console.log(key + " is " + keytype);
+        if (pos === (keys.length - 1)) {
+          client.quit();
+        }
+      });
     });
+  });
 });
+
