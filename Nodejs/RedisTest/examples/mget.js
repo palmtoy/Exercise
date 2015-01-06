49ca2554788fd4a7fcb776fdfaf8e8ff23549537
Zhigang Li <zgli@kabaminc.com>
2015-01-06 16:21:28 +0800

    * mget

--- Nodejs/RedisTest/examples/mget.js
+++ Nodejs/RedisTest/examples/mget.js
@@ -1,5 +1,18 @@
-var client = require("redis").createClient();
+var redis  = require("redis"),
+    client = redis.createClient();
 
-client.mget(["sessions started", "sessions started", "foo"], function (err, res) {
+client.select(1, function(err, rep) {
+  if(err) {
+    return console.log('err = ', err);
+  }
+
+  // client.mset("sessions_started", 100, "foo", "bar", redis.print);
+  client.mset(["sessions_started", 100, "foo", "bar"], redis.print);
+
+  client.mget(["sessions_started", "sessions_started", "foo"], function (err, res) {
     console.dir(res);
+    client.quit();
+  });
+
 });
+
