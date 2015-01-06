cf43f9491e60d710848cde40ad47f5127fead8da
Zhigang Li <zgli@kabaminc.com>
2015-01-06 18:17:13 +0800

    * sort

--- Nodejs/RedisTest/examples/sort.js
+++ Nodejs/RedisTest/examples/sort.js
@@ -6,19 +6,20 @@ client.select(1, function(err, rep) {
     return console.log('err = ', err);
   }
 
-  client.sadd("mylist", 1);
-  client.sadd("mylist", 2);
-  client.sadd("mylist", 3);
+  client.sadd("mylist", 10);
+  client.sadd("mylist", 20);
+  client.sadd("mylist", 30);
 
-  client.set("weight_1", 5);
-  client.set("weight_2", 500);
-  client.set("weight_3", 1);
+  client.set("weight_10", 90);
+  client.set("weight_20", 500);
+  client.set("weight_30", 1);
 
-  client.set("object_1", "foo");
-  client.set("object_2", "bar");
-  client.set("object_3", "qux");
+  client.set("object_10", "foo");
+  client.set("object_20", "bar");
+  client.set("object_30", "qux");
 
-  client.sort("mylist", "by", "weight_*", "get", "object_*", redis.print);
+  // client.sort("mylist", "by", "weight_*", "get", "object_*", redis.print);
+  client.sort("mylist", "by", "weight_*", "get", "object_*", "get", "#",  redis.print);
   // Prints ~ Reply: qux,foo,bar
 
   client.quit();
