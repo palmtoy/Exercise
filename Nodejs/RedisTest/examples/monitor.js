7d9b8d45124fa351481a9c42a7b57effc8f1dde6
Zhigang Li <zgli@kabaminc.com>
2015-01-06 16:30:32 +0800

    * monitor

--- Nodejs/RedisTest/examples/monitor.js
+++ Nodejs/RedisTest/examples/monitor.js
@@ -1,10 +1,19 @@
-var client  = require("../index").createClient(),
+var redis  = require("redis"),
+    client = redis.createClient(),
     util = require("util");
 
-client.monitor(function (err, res) {
-    console.log("Entering monitoring mode.");
+client.select(1, function(err, rep) {
+  if(err) {
+    return console.log('err = ', err);
+  }
+
+  client.monitor(function (err, res) {
+      console.log("Entering monitoring mode ...");
+  });
+
+  client.on("monitor", function (time, args) {
+      console.log(time + ": " + util.inspect(args));
+  });
+
 });
 
-client.on("monitor", function (time, args) {
-    console.log(time + ": " + util.inspect(args));
-});
