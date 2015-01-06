461895ac640ab967fa1004269cfc4d41287b2d43
Zhigang Li <zgli@kabaminc.com>
2015-01-06 17:32:14 +0800

    * pub_sub

--- Nodejs/RedisTest/examples/pub_sub.js
+++ Nodejs/RedisTest/examples/pub_sub.js
@@ -5,37 +5,41 @@ var redis = require("redis"),
 redis.debug_mode = false;
 
 // Most clients probably don't do much on "subscribe".  This example uses it to coordinate things within one program.
+
 client1.on("subscribe", function (channel, count) {
-    console.log("client1 subscribed to " + channel + ", " + count + " total subscriptions");
-    if (count === 2) {
-        client2.publish("a nice channel", "I am sending a message.");
-        client2.publish("another one", "I am sending a second message.");
-        client2.publish("a nice channel", "I am sending my last message.");
-    }
+  console.log("client1 subscribed to " + channel + ", " + count + " total subscriptions");
+  if (count === 2) {
+    console.log("\n");
+    client2.publish("a_nice_channel", "I am sending a message.");
+    client2.publish("another_one", "I am sending a second message.");
+    client2.publish("a_nice_channel", "I am sending my last message.");
+  }
 });
 
 client1.on("unsubscribe", function (channel, count) {
-    console.log("client1 unsubscribed from " + channel + ", " + count + " total subscriptions");
-    if (count === 0) {
-        client2.end();
-        client1.end();
-    }
+  console.log("client1 unsubscribed from " + channel + ", " + count + " total subscriptions");
+  if (count === 0) {
+    client2.end();
+    client1.end();
+  }
 });
 
 client1.on("message", function (channel, message) {
-    console.log("client1 channel " + channel + ": " + message);
-    msg_count += 1;
-    if (msg_count === 3) {
-        client1.unsubscribe();
-    }
+  console.log("client1 channel " + channel + ": " + message + '\n');
+  msg_count += 1;
+  if (msg_count === 3) {
+    console.log("\n");
+    client1.unsubscribe();
+  }
 });
 
 client1.on("ready", function () {
-    // if you need auth, do it here
-    client1.incr("did a thing");
-    client1.subscribe("a nice channel", "another one");
+  // if you need auth, do it here
+  client1.incr("did a thing");
+  client1.subscribe("a_nice_channel", "another_one");
 });
 
 client2.on("ready", function () {
-    // if you need auth, do it here
+  // if you need auth, do it here
 });
+
