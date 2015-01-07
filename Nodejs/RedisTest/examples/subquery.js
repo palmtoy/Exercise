54b86e918358ca318414a47fbb708e54fb2c5f69
Zhigang Li <zgli@kabaminc.com>
2015-01-07 10:45:06 +0800

    * subquery

--- Nodejs/RedisTest/examples/subquery.js
+++ Nodejs/RedisTest/examples/subquery.js
@@ -1,19 +1,29 @@
 var client = require("redis").createClient();
 
 function print_results(obj) {
-    console.dir(obj);
+  console.dir(obj);
 }
 
 // build a map of all keys and their types
-client.keys("*", function (err, all_keys) {
+
+client.select(1, function(err, rep) {
+  if(err) {
+    return console.log('err = ', err);
+  }
+
+  client.keys("*", function (err, all_keys) {
     var key_types = {};
-    
+
     all_keys.forEach(function (key, pos) { // use second arg of forEach to get pos
-        client.type(key, function (err, type) {
-            key_types[key] = type;
-            if (pos === all_keys.length - 1) { // callbacks all run in order
-                print_results(key_types);
-            }
-        });
+      client.type(key, function (err, type) {
+        key_types[key] = type;
+        if (pos === all_keys.length - 1) { // callbacks all run in order
+          print_results(key_types);
+          client.quit();
+        }
+      });
     });
+  });
+
 });
+
