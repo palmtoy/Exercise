243d7b93e4813236ca1311a5e6eb18eceba38baa
Zhigang Li <zgli@kabaminc.com>
2015-01-19 13:46:21 +0800

    * new file

---
+++ Nodejs/FibersTest/error.js
@@ -0,0 +1,28 @@
+var Fiber = require('fibers');
+
+var fn = Fiber(function() {
+  console.log('async work here...');
+  Fiber.yield();
+
+  console.log('still working...');
+  Fiber.yield();
+
+  console.log('just a little bit more...');
+  Fiber.yield();
+
+  throw new Error('oh crap!');
+});
+
+setInterval(function() {
+  try {
+    fn.run();
+  } catch(e) {
+    console.log('safely caught that error!');
+    console.log(e.stack);
+
+    console.log('done!');
+
+    process.exit();
+  }
+}, 500);
+
