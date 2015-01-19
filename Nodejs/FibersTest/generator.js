93f323bfe75881a76402e6a0fca962c705d8f7dc
Zhigang Li <zgli@kabaminc.com>
2015-01-19 10:08:42 +0800

    * optimize

--- Nodejs/FibersTest/generator.js
+++ Nodejs/FibersTest/generator.js
@@ -1,15 +1,18 @@
 var Fiber = require('fibers');
 
 var inc = Fiber(function(start) {
+  console.log('ZZZ ~ start =', start);
   var total = start;
   while (true) {
     console.log('AAA ~ total =', total);
-    total += Fiber.yield(total);
-    console.log('BBB ~ total =', total);
+    var ret = Fiber.yield(total);
+    console.log('BBB ~   ret =', ret);
+    total += ret;
+    console.log('CCC ~ total =', total + '\n');
   }
 });
 
-for (var ii = inc.run(1); ii <= 16; ii = inc.run(2)) {
-  console.log('         ii =', ii + '\n');
+for (var ii = inc.run(2); ii <= 10; ii = inc.run(3)) {
+  console.log('         ii =', ii);
 }
 
