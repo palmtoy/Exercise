90dd8e67d7196a24edf98afdb7d49c4f91eddfbb
Zhigang Li <zgli@kabaminc.com>
2015-01-28 17:27:35 +0800

    * use fiber in mongosync

--- Nodejs/MongoSyncTest/Backup/app-1.js
+++ Nodejs/MongoSyncTest/Backup/app-1.js
@@ -1,9 +1,9 @@
-var Fiber = require('fibers');
+var Future = require('fibers/future');
 var Server = require("mongo-sync").Server;
 
 var server = new Server('127.0.0.1');
 
-Fiber(function() {
+Future.task(function() {
 
   var result = server.db("gam_local").getCollection("user_heroStats").find().toArray();
 
@@ -11,5 +11,5 @@ Fiber(function() {
 
   server.close();
 
-}).run();
+}).detach();
 
