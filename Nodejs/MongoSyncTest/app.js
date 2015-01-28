bc617be001d142457eef12b6c611bd12ffad9fdc
Zhigang Li <zgli@kabaminc.com>
2015-01-28 17:22:32 +0800

    + add new testing file

--- Nodejs/MongoSyncTest/app.js
+++ Nodejs/MongoSyncTest/app.js
@@ -1,10 +1,15 @@
+var Future = require('fibers/future');
 var Server = require("mongo-sync").Server;
 
 var server = new Server('127.0.0.1');
 
-var result = server.db("gam_local").getCollection("users").find().toArray();
+Future.task(function() {
 
-console.log(result);
+  var result = server.db("gam_local").getCollection("user_heroStats").find().toArray();
 
-server.close();
+  console.log(result);
+
+  server.close();
+
+}).detach();
 
