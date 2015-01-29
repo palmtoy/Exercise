fc2c6d29e680e9e9fad213e1c6b92c39b5c7af5a
Zhigang Li <zgli@kabaminc.com>
2015-01-29 13:57:36 +0800

    * add new files

---
+++ Nodejs/HttpTest/CliSvr/cli.js
@@ -0,0 +1,27 @@
+//node.js
+var  HTTPClient = require('HTTPClient.js');
+var options = {
+  hostname: 'localhost',
+  path: '/',
+  port: 8080,
+  secure: false,
+  method: 'GET',
+  headers: {
+    'x-powered-by': 'HTTPClient.js'
+  },
+  body: 'Hi~'
+};
+
+console.log('options', options);
+
+/*
+ * Example 1 - node style
+ */
+var example1 = new HTTPClient(options);
+example1.request('/echo?name=palmtoy', function(err, res, body) {
+    console.log('Example 1');
+    console.log('error:', err);
+    console.log('response:', res);
+    console.log('body:', body);
+    console.log('body(str):', body.toString());
+    });
