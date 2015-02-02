277d64b1979020cd26a58620ea7ed777d0e922a4
Zhigang Li <zgli@kabaminc.com>
2015-02-02 17:47:56 +0800

    + add examples folder

---
+++ Nodejs/CommonNodeTest/examples/hello.js
@@ -0,0 +1,21 @@
+/**
+ * @fileOverview Hello World example.
+ * 
+ * server:
+ * $ common-node hello.js // default port: 8080
+ * $ common-node hello.js -p 8086
+ * $ common-node hello.js -port 8086
+ * $ common-node hello.js -P 8086
+ * 
+ * client:  curl http://localhost:8080
+ * 
+ */
+
+exports.app = function(request) {
+	return {
+		status: 200,
+		headers: {},
+		body: ['Hello World!\n']
+	};
+};
+
