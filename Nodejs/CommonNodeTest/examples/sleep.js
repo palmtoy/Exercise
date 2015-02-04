bacfa2ad5e306839872373cb450e17b50b95fad0
Zhigang Li <zgli@kabaminc.com>
2015-02-04 17:23:39 +0800

    * sleep.js: optimize the code

--- Nodejs/CommonNodeTest/examples/sleep.js
+++ Nodejs/CommonNodeTest/examples/sleep.js
@@ -1,6 +1,15 @@
 /**
  * @fileOverview Sleep example. Sleep for one second before returning a
  *               response.
+ * 
+ * server:
+ * $ common-node hello.js // default port: 8080
+ * $ common-node hello.js -p 8086
+ * $ common-node hello.js -port 8086
+ * $ common-node hello.js -P 8086
+ * 
+ * client:  curl http://localhost:8080
+ * 
  */
 var sleep = require('system').sleep;
 
@@ -12,3 +21,6 @@ exports.app = function(request) {
 		body: ['Hello Sleep!\n']
 	};
 };
+
+console.log('HTTP svr is running on localhost ...');
+
