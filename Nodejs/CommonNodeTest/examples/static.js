b64b3356a1a5bec52380183a00ca889a7ac6fb33
Zhigang Li <zgli@kabaminc.com>
2015-02-04 17:12:45 +0800

    * add some comments

--- Nodejs/CommonNodeTest/examples/static.js
+++ Nodejs/CommonNodeTest/examples/static.js
@@ -1,6 +1,14 @@
 /**
- * @fileOverview Serve a static file from disk. The source code of the current
- *               module, i.e. this file is served.
+ *
+ * @fileOverview Serve a static file from disk. The source code of the current module, i.e. this file is served.
+ * server:
+ * $ common-node hello.js // default port: 8080
+ * $ common-node hello.js -p 8086
+ * $ common-node hello.js -port 8086
+ * $ common-node hello.js -P 8086
+ *
+ * client:  curl http://localhost:8080
+ *
  */
 
 var openRaw = require('fs-base').openRaw;
