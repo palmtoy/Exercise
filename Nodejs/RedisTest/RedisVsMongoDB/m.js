89dc035e556d7f7a2c4583df18c2195372e5c42f
Zhigang Li <zgli@kabaminc.com>
2015-02-28 11:22:14 +0800

    * use prefix/data

--- Nodejs/RedisTest/RedisVsMongoDB/m.js
+++ Nodejs/RedisTest/RedisVsMongoDB/m.js
@@ -1,19 +1,24 @@
 var MongoClient = require('mongodb').MongoClient
-  // , numberOfElements = 50000;
-  , numberOfElements = 5;
+  , perfixStr = 'performance:'
+  , dataStr = require("./data.json");
+
+var numberOfElements = parseInt(process.argv[2]) || 1;
+
+dataStr = JSON.stringify(dataStr);
+var timeStr = Date();
 
 MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
   var collection = db.collection('benchmark');
   collection.ensureIndex({id: 1}, {}, console.log);
   collection.remove({}, function(err) { // to remove any element from the database at first
-    mongoWrite(collection,db);
+    mongoWrite(collection, db);
   });
 });
 
 function mongoWrite(collection, db) {
   console.time('mongoWrite');
   for (var i = 0; i < numberOfElements; i++) {
-    collection.insert({id: i, value: i + " ~ some fantastic value"}, function(err, docs) {
+    collection.insert({id: perfixStr + i, value: i + " ~ " + timeStr + " ~ " + dataStr}, function(err, docs) {
       if(--i === 0) {
         console.timeEnd('mongoWrite');
         mongoRead(collection, db);
@@ -25,7 +30,7 @@ function mongoWrite(collection, db) {
 function mongoRead(collection,db) {
   console.time('mongoRead');
   for (var i = 0; i < numberOfElements; i++) {
-    collection.findOne({id: i}, function(err, results) {
+    collection.findOne({id: perfixStr + i}, function(err, results) {
       if(--i === 0) {
         console.timeEnd('mongoRead');
         db.close();
