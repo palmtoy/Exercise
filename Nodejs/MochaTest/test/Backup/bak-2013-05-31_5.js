808dd1b196dc8b8e50ea0c7c31148d6fcde50747
Zhigang Li <zgli@kabaminc.com>
2015-06-08 14:19:47 +0800

    * format the testing file

--- Nodejs/MochaTest/test/Backup/bak-2013-05-31_5.js
+++ Nodejs/MochaTest/test/Backup/bak-2013-05-31_5.js
@@ -2,24 +2,24 @@ fs = require('fs');
 
 describe('File', function(){
 
-    describe('#readFile()', function(){
+	describe('#readFile()', function(){
 
-        it('should read test-1.js without error', function(done){
-            fs.readFile('test-1.js', function(err, data){
-                if (err) throw err;
-								console.log('1: data = ', data.toString());
-                done();
-            });
-        })
+		it('should read test-1.js without error', function(done){
+			fs.readFile('test-1.js', function(err, data){
+				if (err) throw err;
+				console.log('\t1: data = ', data.toString());
+				done();
+			});
+		})
 
-        it('should read test-2.js without error', function(done){
-            fs.readFile('test-2.js', function(err, data){
-                if (err) throw err;
-								console.log('2: data = ', data.toString());
-                done();
-            });
-        })
+		it('should read test-2.js without error', function(done){
+			fs.readFile('test-2.js', function(err, data){
+				if (err) throw err;
+				console.log('\t2: data = ', data.toString());
+				done();
+			});
+		})
 
-    })
+	})
 })
 
