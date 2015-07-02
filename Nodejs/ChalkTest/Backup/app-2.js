75bb20cebb848fcfbb7c2c13f3310aef91a3d255
Zhigang Li <zgli@kabaminc.com>
2015-07-02 19:34:51 +0800

    + add chalk testing folder

---
+++ Nodejs/ChalkTest/Backup/app-2.js
@@ -0,0 +1,3 @@
+var chalk = require('chalk');
+var error = chalk.bold.red;
+console.log(error('Error!'));
---
+++ Nodejs/ChalkTest/Backup/app-3.js
@@ -0,0 +1,3 @@
+var chalk = require('chalk');
+var name = 'Sindre';
+console.log(chalk.green('Hello %s'), name);
