fef8019948fe3ad6611fa821f9651c3873089e81
Zhigang Li <zgli@kabaminc.com>
2015-08-05 16:43:50 +0800

    * testing undefined null NaN

--- Nodejs/NullUndefinedTest/app.js
+++ Nodejs/NullUndefinedTest/app.js
@@ -1,6 +1,6 @@
 var foo = function(a, b) {
-	console.log('"typeof b" =', typeof b);
 	console.log('b =', b);
+	console.log('"typeof b" =', typeof b);
 	if(b == null) {
 		console.log('"b == null" is TRUE.');
 		b = 100;
@@ -9,5 +9,10 @@ var foo = function(a, b) {
 };
 
 var ret = foo(5);
-
-console.log('\nret =', ret);
+console.log('ret =', ret, '\n');
+var ret = foo(5, undefined);
+console.log('ret =', ret, '\n');
+var ret = foo(5, null);
+console.log('ret =', ret, '\n');
+var ret = foo(5, NaN);
+console.log('ret =', ret, '\n');
