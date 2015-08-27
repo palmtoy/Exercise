ecc80bbbfd9a823a291f26138d77d9da52222af5
Zhigang Li <zgli@kabaminc.com>
2015-08-27 19:28:43 +0800

    * add cnt in svr's log

--- Nodejs/HttpTest/CliSvr/Backup/svr-4.js
+++ Nodejs/HttpTest/CliSvr/Backup/svr-4.js
@@ -5,13 +5,11 @@ curl -v http://127.0.0.1:8080
 var http = require('http');
 var url = require('url');
 
-var n = 0;
-
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
   var queryData = url.parse(req.url, true).query;
-  console.log(++n + ': queryData =' , JSON.stringify(queryData));
+  console.log('queryData =' , JSON.stringify(queryData));
   var now = new Date() + ' ~ ';
   if (queryData.name) {
     res.end(now + 'Hello ' + queryData.name + '\n');
