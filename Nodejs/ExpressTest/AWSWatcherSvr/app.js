e23c369ae60704a4379b9849949b0cdde6d949bf
Zhigang Li <zgli@kabaminc.com>
2015-09-02 13:50:48 +0800

    * make is simple

--- Nodejs/ExpressTest/AWSWatcherSvr/app.js
+++ Nodejs/ExpressTest/AWSWatcherSvr/app.js
@@ -3,24 +3,21 @@ var app = express();
 var myPrint = require('./utils').myPrint;
 
 app.use(function(req, res, next){
-	// myPrint('Object.keys(req) = ', Object.keys(req));
-	// myPrint('Object.keys(res) = ', Object.keys(res));
-	// myPrint('req.method, req.url = ', req.method, req.url);
 	myPrint('req.url = ', req.url);
 	myPrint('req.originalUrl = ', req.originalUrl);
-	myPrint('req._parsedUrl = ', req._parsedUrl);
+	myPrint('req._parsedUrl = ', JSON.stringify(req._parsedUrl));
 
 	var now = Date.now();
 	myPrint('1 ~ now = ', now);
 	next();
 });
 
-app.get('/', function(req, res){
+app.get('/pullCodeAndUpdateSvr', function(req, res){
 	var now = Date();
 	myPrint(now + ' ~ Http request ...');
-	res.send('hi, baby ~ : ' + now);
+	res.send('Yes! Pull code and update svr right now! ~ : ' + now);
 });
 
-app.listen(8080);
+app.listen(8086);
 console.log('Express http server is running ...');
 
