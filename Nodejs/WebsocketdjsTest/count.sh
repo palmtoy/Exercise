623c1070a7f2ea18b75950502d961cc3b50a2616
Zhigang Li <zgli@kabaminc.com>
2015-09-09 11:47:49 +0800

    * add some timestamp format

--- Nodejs/WebsocketdjsTest/count.sh
+++ Nodejs/WebsocketdjsTest/count.sh
@@ -2,7 +2,9 @@
 
 # Define a timestamp function
 timestamp() {
-	date +"%T"
+	# date +"%T" # %H:%M:%S
+	# date +"%s" # 1441770152
+	date +"%r %Y-%m-%d" # 2015-09-09 11:45:51 AM
 }
 
 for ((COUNT = 1; COUNT <= 3600; COUNT++)); do
