1573e3b172f8e7924278076c621def0b581a524d
Zhigang Li <zgli@kabaminc.com>
2015-09-24 21:17:26 +0800

    * use sendStatus func for sending 404

--- Nodejs/EjsTest/Backup/app-1.js
+++ Nodejs/EjsTest/Backup/app-1.js
@@ -1,11 +1,3 @@
-/*
-
-curl http://localhost:3000
-curl http://localhost:3000/post/first-post
-curl http://localhost:3000/api/posts
-
-*/
-
 var express = require('express');
 var fs = require('fs');
 var app = express();
@@ -31,17 +23,11 @@ app.get('/', function(req, res){
 
 app.get('/post/:slug', function(req, res, next){
 	console.log('/post/:slug is running ... req.params.slug =', req.params.slug);
-	var bExistFlag = false;
 	res.locals.posts.forEach(function(post){
 		if (req.params.slug === post.slug){
-			bExistFlag = true;
-			return res.render('post.ejs', { post: post });
+			res.render('post.ejs', { post: post });
 		}
 	})
-
-	if(!bExistFlag) {
-		return res.sendStatus(404);
-	}
 });
 
 app.get('/api/posts', function(req, res){
