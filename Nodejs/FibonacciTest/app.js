a876be13e7935b56600473b8e8aaa5e92b91d72e
Zhigang Li <zgli@kabaminc.com>
2015-09-25 14:12:23 +0800

    * optimize the code of factorial tail recursion

--- Nodejs/FibonacciTest/app.js
+++ Nodejs/FibonacciTest/app.js
@@ -1,16 +1,16 @@
-// tail_recursion.js
-function tailFact(n, p){
+// factorial[math expression: 5! = 120] ~ tail_recursion.js
+function tailFact(n, p) {
   p = p || 1;
-  if(n==0){
+  if(n === 0) {
     console.log('tail fact: ');
     console.dir(process.memoryUsage() );
     return p;
-  }else{
+  } else {
     return tailFact(n-1, p*n);
   }
 }
 
-var cnt = parseInt(process.argv[2]);
+var cnt = parseInt(process.argv[2]) || 1;
 var ret = tailFact(cnt);
 console.log('ret = ', ret);
 
