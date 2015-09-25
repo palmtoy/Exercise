d4a7ff97c7e33a4b3a6bd50ce37c57b65f1c7e4c
Zhigang Li <zgli@kabaminc.com>
2015-09-25 14:13:17 +0800

    * add new testing file in FibonacciTest folder

---
+++ Nodejs/FibonacciTest/Backup/app-6.js
@@ -0,0 +1,16 @@
+// tail_recursion.js
+function tailFact(n, p){
+  p = p || 1;
+  if(n==0){
+    console.log('tail fact: ');
+    console.dir(process.memoryUsage() );
+    return p;
+  }else{
+    return tailFact(n-1, p*n);
+  }
+}
+
+var cnt = parseInt(process.argv[2]);
+var ret = tailFact(cnt);
+console.log('ret = ', ret);
+
