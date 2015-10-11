e45b975f4428583f490d1d95038b77bee9c84b3c
Zhigang Li <zgli@kabaminc.com>
2015-10-11 14:46:20 +0800

    * use Formula to update spreadsheet

--- Nodejs/EditGoogleSpreadsheetTest/Backup/app-2.js
+++ Nodejs/EditGoogleSpreadsheetTest/Backup/app-2.js
@@ -16,31 +16,22 @@ Spreadsheet.load({
 		throw err;
 	}
 
+	spreadsheet.receive(function(err, rows, info) {
+		if (err) {
+			throw err;
+		}
+		console.dir(rows);
+		console.dir(info);
+	});
+
 	spreadsheet.add(
 		{ 4: { 1: 'work', 2: 'work', 3: 'enjoy' } }
 	);
-
-	spreadsheet.add({
-		5: {
-			1: { name: "a", val: 100 }, // '66' though tagged as "a" 
-			2: { name: "b", val: 200 }, // '88' though tagged as "b" 
-			3: "={{ a }}+{{ b }}"      // forumla adding row5,col1 with row5,col2 => '=A5+B5' 
-		}
-	});
-
 	spreadsheet.send(function(err) {
 		if (err) {
 			throw err;
 		}
-		console.log("Updated the 4th/5th lines ...");
-
-		spreadsheet.receive(function(err, rows, info) {
-			if (err) {
-				throw err;
-			}
-			console.dir(rows);
-			console.dir(info);
-		});
+		console.log("Updated the 4th line ...");
 	});
 });
 
