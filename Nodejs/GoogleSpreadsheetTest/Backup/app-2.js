9915ec36eae9f83a6b31c476d663b3ec061a73ee
Zhigang Li <zgli@kabaminc.com>
2015-10-11 21:21:05 +0800

    * read published google spreadsheet without auth

--- Nodejs/GoogleSpreadsheetTest/Backup/app-2.js
+++ Nodejs/GoogleSpreadsheetTest/Backup/app-2.js
@@ -4,15 +4,8 @@ var GoogleSpreadsheet = require("google-spreadsheet");
 // spreadsheet key is the long id in the sheets URL 
 var my_sheet = new GoogleSpreadsheet('1b0u48zSjGrZ8ePe7Sa-3QuT9Mty4nG6WPslY180GpJc');
 
-/*
-my_sheet.getRows( 1, function(err, row_data){
-	console.log( 'Read published spreadsheet: pulled in '+ row_data.length + ' rows' );
-	console.log('row_data =>');
-	console.dir(row_data);
-});
-*/
+// With auth -- read + write 
 
-// With auth -- read + write
 var privateKey = fs.readFileSync('./id_rsa_for_google');
 var creds = {
 	client_email: '376314952996-682nse4maq7opmjop6294mn4fhtuab8r@developer.gserviceaccount.com',
@@ -22,7 +15,7 @@ var creds = {
 my_sheet.useServiceAccountAuth(creds, function(err){
 	// getInfo returns info about the sheet and an array or "worksheet" objects 
 	my_sheet.getInfo( function( err, sheet_info ){
-		console.log( '\n' + sheet_info.title + ' is loaded.\n' );
+		console.log( sheet_info.title + ' is loaded.\n' );
 		console.dir(sheet_info);
 		console.log('\n');
 
