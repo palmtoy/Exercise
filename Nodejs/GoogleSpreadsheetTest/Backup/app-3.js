47956cff6a60117c2b7a5fa62cf3a0128a239733
Zhigang Li <zgli@kabaminc.com>
2015-10-11 22:03:26 +0800

    * update spreadsheet

--- Nodejs/GoogleSpreadsheetTest/Backup/app-3.js
+++ Nodejs/GoogleSpreadsheetTest/Backup/app-3.js
@@ -5,12 +5,12 @@ var GoogleSpreadsheet = require("google-spreadsheet");
 var my_sheet = new GoogleSpreadsheet('1b0u48zSjGrZ8ePe7Sa-3QuT9Mty4nG6WPslY180GpJc');
 
 /*
-	 my_sheet.getRows( 1, function(err, row_data){
-	 console.log( 'Read published spreadsheet: pulled in '+ row_data.length + ' rows' );
-	 console.log('row_data =>');
-	 console.dir(row_data);
-	 });
-	 */
+my_sheet.getRows( 1, function(err, row_data){
+	console.log( 'Read published spreadsheet: pulled in '+ row_data.length + ' rows' );
+	console.log('row_data =>');
+	console.dir(row_data);
+});
+*/
 
 // With auth -- read + write
 var privateKey = fs.readFileSync('./id_rsa_for_google');
@@ -29,10 +29,7 @@ my_sheet.useServiceAccountAuth(creds, function(err){
 		// use worksheet object if you want to stop using the # in your calls 
 		var sheet1 = sheet_info.worksheets[0];
 		sheet1.getRows( function( err, rows ){
-			console.dir(rows[0]);
-			rows[0].macosx = 'NetEase';
-			rows[0].save(); //async and takes a callback
-			// console.dir(rows);
+			console.dir(rows);
 		});
 	});
 });
