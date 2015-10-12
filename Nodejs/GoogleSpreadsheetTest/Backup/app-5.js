a7487509f39e7ba529dd082b834053c617fd2f82
Zhigang Li <zgli@kabaminc.com>
2015-10-12 10:29:36 +0800

    * use google-oauth-jwt and google-spreadsheet together

--- Nodejs/GoogleSpreadsheetTest/Backup/app-5.js
+++ Nodejs/GoogleSpreadsheetTest/Backup/app-5.js
@@ -1,28 +1,35 @@
 var fs = require('fs');
-var GoogleOAuthJWT = require('google-oauth-jwt');
 var GoogleSpreadsheet = require("google-spreadsheet");
+
+// spreadsheet key is the long id in the sheets URL 
+var my_sheet = new GoogleSpreadsheet('1b0u48zSjGrZ8ePe7Sa-3QuT9Mty4nG6WPslY180GpJc');
+
+/*
+	 my_sheet.getRows( 1, function(err, row_data){
+	 console.log( 'Read published spreadsheet: pulled in '+ row_data.length + ' rows' );
+	 console.log('row_data =>');
+	 console.dir(row_data);
+	 });
+	 */
 
 // With auth -- read + write
 var privateKey = fs.readFileSync('./id_rsa_for_google');
-var oauth = {
-	email: '376314952996-682nse4maq7opmjop6294mn4fhtuab8r@developer.gserviceaccount.com',
-	key: privateKey,
-	scopes: ['https://spreadsheets.google.com/feeds']
-};
+var creds = {
+	client_email: '376314952996-682nse4maq7opmjop6294mn4fhtuab8r@developer.gserviceaccount.com',
+	private_key: privateKey
+}
 
-GoogleOAuthJWT.authenticate(oauth, function(err, token) {
-	var sheetId = '1b0u48zSjGrZ8ePe7Sa-3QuT9Mty4nG6WPslY180GpJc';
-	var my_sheet = new GoogleSpreadsheet(sheetId, {type : 'Bearer', value: token});
-
+my_sheet.useServiceAccountAuth(creds, function(err){
+	// getInfo returns info about the sheet and an array or "worksheet" objects 
 	my_sheet.getInfo( function( err, sheet_info ){
 		console.log( '\n' + sheet_info.title + ' is loaded.\n' );
 		console.dir(sheet_info);
+
 		// use worksheet object if you want to stop using the # in your calls 
 		var sheet1 = sheet_info.worksheets[0];
 		sheet1.getRows( function( err, rows ){
 			console.log('\n');
 			console.dir(rows[0]);
-
 			rows[0].macosx = 'NetEase';
 			rows[0].save(function() {
 				my_sheet.getInfo( function( err, sheet_info ){
@@ -33,7 +40,8 @@ GoogleOAuthJWT.authenticate(oauth, function(err, token) {
 					});
 				});
 			});
-		});
+		}); //async and takes a callback
+		// console.dir(rows);
 	});
 });
 
