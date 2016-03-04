16a6fcea3068a2858594e0b224b7a0eecfbec2f8
Zhigang Li(Will) <zgli@kabaminc.com>
2016-03-04 10:29:12 +0800

    * testing some new special chars

--- Nodejs/RegExpTest/app.js
+++ Nodejs/RegExpTest/app.js
@@ -1,6 +1,6 @@
 var nameRegexInv = new RegExp("((?=[\x20-\xff]+)[^A-Za-z0-9-_])|(?=[\u2016-\u29FB]+)");
 
-var name = ' !@#$%^&*()+|~[];\',./{}:\"<>?£¥₩€aZ09-_';
+var name = '€£¥₩¤♡♥¡¿º•○●□■◇◆♧♣▲▼▶◀↑↓←→☆★▪';
 
 for(var i = 0; i < name.length; i++) {
 	var ret = nameRegexInv.test(name[i]);
