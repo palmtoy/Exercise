1e7ff280faa220fbd912007b9a67de74bc547796
Zhigang Li(Will) <zgli@kabaminc.com>
2016-07-21 20:25:21 +0800

    + insert some more data into table user

--- Nodejs/MongodbTest/app.js
+++ Nodejs/MongodbTest/app.js
@@ -1,21 +1,32 @@
 var MongoClient = require('mongodb').MongoClient
-, format = require('util').format;
+	, format = require('util').format;
 
 MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
-    if(err) throw err;
+	if(err) throw err;
 
-    var collection = db.collection('test_insert');
-    collection.insert({a:2}, function(err, docs) {
+	var collection = db.collection('user');
 
-      collection.count(function(err, count) {
-        console.log(format("count = %s", count));
-      });
+	for(var i = 0; i < 100; i++) {
+		var k = i + 1;
+		var name = 'Tom_' + k;
+		var ts = Date() + ' ~ ' + k;
+		collection.insert({uid: k, name: name, ts: ts}, function(err, docs) {});
+	}
 
-      // Locate all the entries using find
-      collection.find().toArray(function(err, results) {
-        console.dir(results);
-        // Let's close the db
-        db.close();
-      });
-    });
-    })
+	setTimeout(function() {
+		collection.count(function(err, count) {
+			console.log(format("count = %s", count));
+		});
+
+		// Locate all the entries using find
+		collection.find().toArray(function(err, results) {
+			console.dir(results);
+		});
+
+		setTimeout(function() {
+			// Let's close the db
+			db.close();
+		}, 1000);
+	}, 2000);
+});
+
