3c99880a82656a9860193e9962273b845dffe794
Zhigang Li(Will) <zgli@kabaminc.com>
2016-08-18 04:54:52 -0700

    * use setInterval

--- Nodejs/ConnectTest/cli.js
+++ Nodejs/ConnectTest/cli.js
@@ -1,38 +1,39 @@
 var request = require('request');
 
-
-request('http://localhost:3000', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+var tmpInterval = parseInt(process.argv[2]);
 
 
-request('http://localhost:3000/hi', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+setInterval(function() {
+	request('http://localhost:3000', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+		}
+	});
 
+	request('http://localhost:3000/hi', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+		}
+	});
 
-request.post({
-  headers: {'Content-type': 'application/x-www-form-urlencoded', charset: 'utf-8'},
-  url: 'http://localhost:3000/wow',
-  body: 'user=will'
-}, function(error, response, body){
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+	request.post({
+		headers: {'Content-type': 'application/x-www-form-urlencoded', charset: 'utf-8'},
+		url: 'http://localhost:3000/wow',
+		body: 'user=will'
+	}, function(error, response, body){
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+		}
+	});
 
-
-request.post({
-  headers: {'Content-type': 'application/x-www-form-urlencoded', charset: 'utf-8'},
-  url: 'http://localhost:3000/qs',
-  body: 'user[name][first]=will&user[email]=zgli@kabaminc.com'
-}, function(error, response, body){
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+	request.post({
+		headers: {'Content-type': 'application/x-www-form-urlencoded', charset: 'utf-8'},
+		url: 'http://localhost:3000/qs',
+		body: 'user[name][first]=will&user[email]=zgli@kabaminc.com'
+	}, function(error, response, body){
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+		}
+	});
+}, tmpInterval);
 
