8e5167375b68d8c56d247df47981ee42898dc1eb
Zhigang Li(Will) <zgli@kabaminc.com>
2016-11-02 20:07:52 +0800

    * use task_queue and time-consuming tasks

---
+++ Nodejs/RabbitMQTest/20161102/new_task.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+/* 
+	1) ./worker.js
+	2) ./new_task.js '.....'
+	ps: the param '.....' is just for new_task.js
+*/
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://UbuntuVB', function(err, conn) {
+	conn.createChannel(function(err, ch) {
+		var q = 'task_queue';
+		var msg = process.argv.slice(2).join(' ') || "Hello World!";
+
+		ch.assertQueue(q, {durable: true});
+		ch.sendToQueue(q, new Buffer(msg), {persistent: true});
+		console.log(" [x] Sent '%s'", msg);
+
+		setTimeout(function(){
+			conn.close(); process.exit(0)
+		}, 10);
+	});
+});
+
