4db27770b549d22af4de18bde7c15811efbbe47f
Zhigang Li(Will) <zgli@kabaminc.com>
2016-11-14 18:47:52 +0800

    + add new folder: 20161115

---
+++ Nodejs/RabbitMQTest/20161115/worker.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+/* 
+	1) ./worker.js
+	2) ./new_task.js '.....'
+	ps: the param '.....' is just for new_task.js
+*/
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://UbuntuVB', function(err, conn) {
+	conn.createChannel(function(err, ch) {
+		var q = 'task_queue';
+
+		ch.assertQueue(q, {durable: true});
+    ch.prefetch(1);
+		console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+
+		ch.consume(q, function(msg) {
+			var str = msg.content.toString();
+			var secs = str.split('.').length - 1;
+			console.log(" [x] Received %s ~ %s ~ will last for %d seconds.", str, Date(), secs);
+
+			setTimeout(function() {
+				console.log(" [x]     Done %s ~ %s\n", str, Date());
+				ch.ack(msg);
+			}, secs * 1000);
+		}, {noAck: false});
+	});
+});
+
