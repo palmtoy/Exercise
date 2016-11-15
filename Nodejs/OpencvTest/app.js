681184013cffc71b39bb1acc7850dd94c9da0474
Zhigang Li(Will) <zgli@kabaminc.com>
2016-11-15 16:06:48 +0800

    + add OpencvTest folder

---
+++ Nodejs/OpencvTest/app.js
@@ -0,0 +1,21 @@
+var cv = require('opencv');
+
+/*
+	1) put the picture 'mona.png(Mona Lisa)' at Downloads folder
+	2) node app.js
+	3) open out.jpg at Downloads folder
+*/
+
+var dlDir = process.env.HOME + '/Downloads/';
+
+cv.readImage(dlDir + 'mona.png', function(err, im){
+	im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+		for (var i = 0; i < faces.length; i++){
+			var x = faces[i]
+			im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+		}
+
+		im.save(dlDir + 'out.jpg');
+	});
+})
+
