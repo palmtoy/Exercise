6f94f44704ee542c1778e153b4293f338f6a232d
Zhigang Li(Will) <zgli@kabaminc.com>
2016-11-17 20:02:57 +0800

    * use async in mario.js

--- Nodejs/OpencvTest/mario.js
+++ Nodejs/OpencvTest/mario.js
@@ -1,46 +1,36 @@
-var cv = require('opencv');
+var cv = require('opencv'),
+	async = require('async');
 
-var colorDict = [
-		[0, 255, 0], // green
-		[255, 0, 0], // blue
-		[0, 0, 255] // red
-	];
+
 var targetFilename = './images/game_scene.jpg';
-var templateFileList = [
-		'./images/game_mario.jpg',
-		'./images/game_coin.jpg',
-		'./images/game_block.jpg'
+var templateList = [
+		{idx: 1, img: './images/game_mario.jpg', color: [0, 255, 0]}, // green
+		{idx: 2, img: './images/game_coin.jpg', color: [255, 0, 0]}, // blue
+		{idx: 3, img: './images/game_block.jpg', color: [0, 0, 255]}, // red
 	];
 
 cv.readImage(targetFilename, function(err, imgTarget){
-
-	function _doDetect(j) {
-		var templateFilename = templateFileList[j];
-		cv.readImage(templateFilename, function(err, imgTemplate){
+	async.each(templateList, function(tObj, cb) {
+		cv.readImage(tObj.img, function(err, imgTemplate){
 			var TM_CCORR_NORMED = 3;
 			var res = imgTarget.matchTemplateByMatrix(imgTemplate, TM_CCORR_NORMED);
 			var minMax = res.minMaxLoc();
 			var topLeft = minMax.maxLoc;
-			console.log('%d: topLeft = %j', i, topLeft, '\n');
+			console.log('%d: topLeft = %j', tObj.idx, topLeft);
 
 			var tmpWidth = imgTemplate.width();
 			var tmpHeight = imgTemplate.height();
-			console.log('%d: size = {width: %d, height: %d}', i, tmpWidth, tmpHeight, '\n');
+			console.log('%d: size = {width: %d, height: %d}', tObj.idx, tmpWidth, tmpHeight, '\n');
 
-			imgTarget.rectangle([topLeft.x, topLeft.y], [tmpWidth, tmpHeight], colorDict[j], 2);
+			var LINE_WIDTH = 2;
+			imgTarget.rectangle([topLeft.x, topLeft.y], [tmpWidth, tmpHeight], tObj.color, LINE_WIDTH);
+			cb();
 		});
-	}
-
-	var len = templateFileList.length;
-
-	for(var i = 0; i < len; i++) {
-		_doDetect(i);
-	}
-
-	setTimeout(function() {
+	}, function(err) {
 		var outputFilename = './game_scene-rectangle.jpg';
 		imgTarget.save(outputFilename);
 		console.log('Image saved to ' + outputFilename);
-	}, 2000);
+	});
+
 });
 
