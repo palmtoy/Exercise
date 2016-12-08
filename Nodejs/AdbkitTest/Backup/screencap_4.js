a791a265657afc92e4ce150ce9d415a255189807
Zhigang Li(Will) <zgli@kabaminc.com>
2016-12-08 20:33:57 +0800

    * output img directly

--- Nodejs/AdbkitTest/Backup/screencap_4.js
+++ Nodejs/AdbkitTest/Backup/screencap_4.js
@@ -9,16 +9,26 @@ client.listDevices()
 		return Promise.map(devices, function(device) {
 			return client.screencap(device.id)
 			.then(function(imgStream) {
+				imgStream.pipe(fs.createWriteStream('myscreen-' + device.id + '-origin.png'));
+
 				imgStream
 				.pipe(new PNG({
 					filterType: 4
 				}))
 				.on('parsed', function() {
+					for (var y = 0; y < this.height; y++) {
+						for (var x = 0; x < this.width; x++) {
+							var idx = (this.width * y + x) << 2;
+							// invert color 
+							this.data[idx] = 255 - this.data[idx];
+							this.data[idx+1] = 255 - this.data[idx+1];
+							this.data[idx+2] = 255 - this.data[idx+2];
+							// and reduce opacity 
+							this.data[idx+3] = this.data[idx+3] >> 1;
+						}
+					}
 					this.pack().pipe(fs.createWriteStream('myscreen-' + device.id + '-parsed.png'));
-				})
-				.on('metadata', function(metadata) {
-					console.log('metadata =', JSON.stringify(metadata))
-				})
+				});
 			});
 		})
 	})
