414f21f7af77731d2633243673c4f682976220bf
lzg <lzg@pwrd.com>
2017-03-27 15:17:33 +0800

    + add redistest folder

---
+++ Java/redistest/src/main/java/com/pwrd/redistest/App.java
@@ -0,0 +1,14 @@
+package com.pwrd.redistest;
+
+import redis.clients.jedis.Jedis;
+
+public class App {
+	public static void main(String[] args) {
+		//连接本地的 Redis 服务
+		Jedis jedis = new Jedis("localhost");
+		System.out.println("Connection to server sucessfully.");
+		//查看服务是否运行
+		System.out.println("Server is running: " + jedis.ping());
+	}
+}
+
