976664bdcbe8f6bf9548fcc087dae3bdc79d03eb
lizhigang <lizhigang@pwrd.com>
2017-04-25 13:54:41 +0800

    * check the type of return value of jsonObject.get

--- Java/gsontest/src/main/java/com/pwrd/gsontest/App.java
+++ Java/gsontest/src/main/java/com/pwrd/gsontest/App.java
@@ -8,9 +8,11 @@ import net.sf.json.JSONObject;
 public class App
 {
 	public static void main( String[] args ) {
-		String json = "{id=\"1\", name=\"json\"}";
+		String json = "{id=\"1\", name=\"palmtoy\"}";
 		JSONObject jsonObject = JSONObject.fromObject(json);
 		System.out.println(jsonObject);
+		Object str = jsonObject.get("name");
+		System.out.println(str);
 	}
 }
 
