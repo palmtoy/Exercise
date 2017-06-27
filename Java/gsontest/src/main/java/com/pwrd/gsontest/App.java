04d6270d9168e9dcc7a3db35fb1ecf978301c3cc
lizhigang <lizhigang@pwrd.com>
2017-06-27 15:56:07 +0800

    * read json string from file and convert it to json object

--- Java/gsontest/src/main/java/com/pwrd/gsontest/App.java
+++ Java/gsontest/src/main/java/com/pwrd/gsontest/App.java
@@ -1,17 +1,23 @@
 package com.pwrd.gsontest;
 
 import net.sf.json.JSONObject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  *
  */
 public class App
 {
-	public static void main( String[] args ) {
-		String json = "{id=\"1\", name=\"palmtoy\"}";
-		JSONObject jsonObject = JSONObject.fromObject(json);
+	public static void main( String[] args ) throws IOException {
+		String filePath = App.class.getResource("/").getPath() + "data.json";
+		String strJson = new String(Files.readAllBytes(Paths.get(filePath)));
+
+		JSONObject jsonObject = JSONObject.fromObject(strJson);
 		System.out.println(jsonObject);
-		Object str = jsonObject.get("name");
+
+		Object str = jsonObject.get("charName");
 		System.out.println(str);
 	}
 }
