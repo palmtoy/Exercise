a1f76bfbe54e76f2ce5e4a23ea65c5213a2335ba
lizhigang <lizhigang@pwrd.com>
2017-07-01 11:46:45 +0800

    + use index.html as the home page

--- Java/SpringTest/springbootest/src/main/java/pwrd/springbootest/SampleController.java
+++ Java/SpringTest/springbootest/src/main/java/pwrd/springbootest/SampleController.java
@@ -1,22 +1,13 @@
 package pwrd.springbootest;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return new Date() + " ~ Hello World!";
-    }
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
     }
