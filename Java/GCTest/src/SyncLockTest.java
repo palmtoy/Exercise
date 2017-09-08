fee5531daa8823f704231c75687dab22b98bda0d
lizhigang <lizhigang@pwrd.com>
2017-09-08 10:54:40 +0800

    * print some log for every running thread

--- Java/GCTest/src/SyncLockTest.java
+++ Java/GCTest/src/SyncLockTest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,10 +9,12 @@ import java.util.concurrent.Executors;
 public class SyncLockTest {
 
 	private static int value;
+	private static int threadNum = 4;
+	private static int[] cntArray = new int[threadNum];
 
 	public static void main(String[] args) {
 		long begin = System.currentTimeMillis();
-		ExecutorService executorService = Executors.newFixedThreadPool(4);
+		ExecutorService executorService = Executors.newFixedThreadPool(threadNum);
 		int num = 10000000;
 		for(int i = 0; i < num; i++) {
 			executorService.execute(new Runnable() {
@@ -36,7 +39,8 @@ public class SyncLockTest {
 							VM options: -Xms3g -Xmx3g -Xmn2g
 							SyncLockTest ~ value: 10000000  times: 1839ms
 						*/
-						System.out.println("SyncLockTest ~ value: " + v + "  times: " + (end - begin) + "ms");
+						System.out.println("SyncLockTest ~ value: " + v + "  times: " + (end - begin) + "ms\n");
+						System.out.println("CntArray =" + Arrays.toString(cntArray));
 						Scanner in = new Scanner(System.in);
 						String s = in.nextLine();
 						System.exit(0);
@@ -48,6 +52,14 @@ public class SyncLockTest {
 	}
 
 	private static synchronized int incrementAndGet() {
+		String threadName = Thread.currentThread().getName();
+		if(value % 2000000 == 0) {
+			System.out.println("IncrementAndGet ~ ThreadName = " + threadName);
+			System.out.println("IncrementAndGet ~ value = " + value + "\n");
+		}
+		String tmp = threadName.substring(threadName.length()-1);
+		int idx = Integer.parseInt(tmp);
+		cntArray[idx-1]++;
 		return ++value;
 	}
 
