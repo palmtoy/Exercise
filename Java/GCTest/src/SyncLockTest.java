9f61271ea5311af0d949fe7395c98262b92010a2
lizhigang <lizhigang@pwrd.com>
2017-09-11 13:57:04 +0800

    * add some vm options tests

--- Java/GCTest/src/SyncLockTest.java
+++ Java/GCTest/src/SyncLockTest.java
@@ -34,11 +34,11 @@ public class SyncLockTest {
 							Without VM options:
 							SyncLockTest ~ value: 10000000  times: 5913ms
 
-							VM options: -Xms3g -Xmx3g -Xmn2g
+							VM options: -XX:-UseBiasedLocking -Xms3g -Xmx3g -Xmn2g
 							SyncLockTest ~ value: 10000000  times: 1933ms
 							CntArray =[2427774, 2559846, 2595576, 2416804]
 
-							VM options: -XX:-UseBiasedLocking -Xms3g -Xmx3g -Xmn2g
+							VM options: -XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0 -Xms3g -Xmx3g -Xmn2g
 							SyncLockTest ~ value: 10000000  times: 2012ms
 							CntArray =[2512125, 2493540, 2555175, 2439160]
 
@@ -49,6 +49,10 @@ public class SyncLockTest {
 
 							VM options: -Xms3g -Xmx3g -Xmn2g
 							SyncLockTest ~ value: 10000000  times: 1839ms
+
+							VM options: -XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0 -Xms3g -Xmx3g -Xmn2g
+							SyncLockTest ~ value: 10000000  times: 1482ms
+							CntArray =[2590096, 2275418, 2208938, 2925548]
 						*/
 						System.out.println("SyncLockTest ~ value: " + v + "  times: " + (end - begin) + "ms\n");
 						System.out.println("CntArray =" + Arrays.toString(cntArray));
