3bf97381be02acc9bb748cdd803ab34e5c3d8b5c
lizhigang <lizhigang@pwrd.com>
2017-11-21 21:39:02 +0800

    + add HelloWorld folder under env:vscode

---
+++ CPlusPlus/HelloWorld/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string str = "Hello World ~" ;
+	cout << str << endl;
+	int tmpV = 999;
+	++tmpV;
+	cout << "tmpV = " << tmpV << endl;
+	return 0;
+}
