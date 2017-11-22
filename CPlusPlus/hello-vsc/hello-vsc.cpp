e75228b6d74ce18d6df0cc01d43e3fbce86cdeaa
lizhigang <lizhigang@pwrd.com>
2017-11-22 14:53:30 +0800

    + debug the code with vsc

--- CPlusPlus/hello-vsc/hello-vsc.cpp
+++ CPlusPlus/hello-vsc/hello-vsc.cpp
@@ -2,6 +2,12 @@
 
 int main(int argc, const char *argv[])
 {
+  int tmpV = 9527;
+  tmpV++;
+  std::cout << "tmpV =" << tmpV << std::endl;
+  for(int tmpI = 0; tmpI < 3; tmpI++) {
+    std::cout << "tmpI =" << tmpI << std::endl;
+  }
   std::cout << "hello Visual Studio Code! :)" << '\n';
   return 0;
 }
