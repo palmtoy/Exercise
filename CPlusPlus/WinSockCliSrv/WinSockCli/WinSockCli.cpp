c77f03e062f2645a12f5f62a717b1ffcfef5f0b3
Li Zhigang <lizg@mail.jj.cn>
2018-01-23 19:29:16 +0800

    * send time-now to server

--- CPlusPlus/WinSockCliSrv/WinSockCli/WinSockCli.cpp
+++ CPlusPlus/WinSockCliSrv/WinSockCli/WinSockCli.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <chrono>
+#include <string>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
+using namespace std::chrono;
 
 int main(int argc, char *argv[])
 {
-	string buffer = "Hi ~";
+	string strBuffer = "Hi ~ ";
+	time_t tt = system_clock::to_time_t(system_clock::now());
+	strBuffer += std::to_string(tt);
 
 	if(argc > 1)
 	{
-		buffer = argv[1];
+		strBuffer = argv[1];
 	}
 
 	WSADATA wsa_data;
@@ -26,19 +31,19 @@ int main(int argc, char *argv[])
 	LPCWSTR pszAddrString = wstr.c_str();
 	InetPton(AF_INET, pszAddrString, &addr.sin_addr.s_addr);
 
+	USHORT sinPort = 8086;
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(5555);
+	addr.sin_port = htons(sinPort);
 
 	connect(server, reinterpret_cast<SOCKADDR *>(&addr), sizeof(addr));
-	cout << "Connected to server!" << endl;
+	cout << "Connected to server ..." << endl;
 
-	send(server, buffer.c_str(), sizeof(buffer), 0);
-	cout << "Message sent!" << endl;
+	send(server, strBuffer.c_str(), sizeof(strBuffer), 0);
+	cout << "Message sent -> " << strBuffer.c_str() << endl;
 
 	closesocket(server);
 	WSACleanup();
 	cout << "Socket closed." << endl << endl;
 
-	char c;
-	cin >> c;
+	Sleep(3000);
 }
--- CPlusPlus/WinSockCliSrv/WinSockSrv/WinSockSrv.cpp
+++ CPlusPlus/WinSockCliSrv/WinSockSrv/WinSockSrv.cpp
@@ -16,14 +16,15 @@ int main()
 	WSAStartup(MAKEWORD(2, 2), &wsa_data);
 	const auto server = socket(AF_INET, SOCK_STREAM, 0);
 
+	USHORT sinPort = 8086;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(5555);
+	server_addr.sin_port = htons(sinPort);
 
 	::bind(server, reinterpret_cast<SOCKADDR *>(&server_addr), sizeof(server_addr));
 	listen(server, 0);
 
-	cout << "Listening for incoming connections..." << endl;
+	cout << "WinSock server is running on port " << sinPort << " ..." << endl;
 
 	int client_addr_size = sizeof(client_addr);
 
@@ -49,7 +50,7 @@ void on_client_connect(SOCKET client)
 {
 	char buffer[1024];
 
-	cout << "Client connected!" << endl;
+	cout << "\nClient connected ..." << endl;
 	recv(client, buffer, sizeof(buffer), 0);
 
 	cout << "Client says: " << buffer << endl;
