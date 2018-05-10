c6e3ff695b376b536fda137f81c3979b38d00f74
ZhiGangLi <ZhiGangLi@sf-express.com>
2018-05-10 19:53:39 +0800

    + add new testing file:app-x.js

---
+++ Nodejs/KoaTest/app-x.js
@@ -0,0 +1,33 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+const port = 3000;
+
+app.listen(port);
+
+console.log(new Date() + ' ~ Koa http server is running on port ' + port + ' ...');
+
