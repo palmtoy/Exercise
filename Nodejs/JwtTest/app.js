af30443543236d5a33048bea2459327b38697efc
ZhiGangLi <ZhiGangLi@sf-express.com>
2018-07-11 17:41:19 +0800

    * polish the code

--- Nodejs/JwtTest/app.js
+++ Nodejs/JwtTest/app.js
@@ -4,21 +4,15 @@ const Koa = require('koa');
 const koajwt = require('koa-jwt');
 const jwt = require('jsonwebtoken');
 
-const profile = {
-	id: 186163
-};
+const profile = { id: 186163 };
 const secretCode = 'd73b04b0e696b0945283defa3eee4538';
-
 const TOKEN = jwt.sign(profile, secretCode, { expiresIn: 8 * 60 * 60 }); // 8 hours
 
-console.log('Starting koa-jwt test server on http://localhost:3000/');
-console.log('');
-console.log('You can test the server by issuing curl commands like the following:');
-console.log('');
+console.log('Starting koa-jwt test server on http://localhost:3000/\n');
+console.log('You can test the server by issuing curl commands like the following:\n');
 console.log('  curl http://localhost:3000/public/foo            # should succeed (return "unprotected")');
 console.log('  curl http://localhost:3000/api/foo               # should fail (return "401 Unauthorized ...")');
-console.log('  curl -H "Authorization: Bearer ' + TOKEN + '" http://localhost:3000/api/foo   # should succeed (return "protected")');
-console.log('');
+console.log('  curl -H "Authorization: Bearer ' + TOKEN + '" http://localhost:3000/api/foo   # should succeed (return "protected")\n');
 
 const app = new Koa();
 
@@ -53,4 +47,3 @@ app.use(ctx => {
 });
 
 app.listen(3000);
-
