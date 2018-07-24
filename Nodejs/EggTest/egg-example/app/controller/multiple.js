d2d51f003450d2aeabfce305b46ce2e41c8320c2
ZhiGangLi <ZhiGangLi@sf-express.com>
2018-07-24 10:28:09 +0800

    * restore the dump data after uploading

--- Nodejs/EggTest/egg-example/app/controller/multiple.js
+++ Nodejs/EggTest/egg-example/app/controller/multiple.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const Controller = require('egg').Controller;
 const pump = require('mz-modules/pump');
+const mongodbRestore = require('mongodb-restore');
 
 class UploadMultipleController extends Controller {
   async show() {
@@ -13,6 +14,7 @@ class UploadMultipleController extends Controller {
   async upload() {
     const parts = this.ctx.multipart({ autoFields: true });
     const files = [];
+    let restoreTar = null;
 
     let stream;
     while ((stream = await parts()) != null) {
@@ -21,6 +23,20 @@ class UploadMultipleController extends Controller {
       const writeStream = fs.createWriteStream(target);
       await pump(stream, writeStream);
       files.push(filename);
+      if (!restoreTar && filename) {
+        restoreTar = target;
+      }
+    }
+
+    if (restoreTar) {
+      mongodbRestore({
+        uri: 'mongodb://127.0.0.1/trace-source-data', // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
+        root: path.dirname(restoreTar), // read backup(s) file(s) from this dir
+        tar: path.basename(restoreTar), // restore backup(s) tar file(s) from this dir
+        metadata: true,
+        drop: true, // drop entire database before restore backup
+        logger: '/tmp/mongodb-restore.log',
+      });
     }
 
     await this.ctx.render('page/multiple_result.html', {
@@ -31,3 +47,4 @@ class UploadMultipleController extends Controller {
 }
 
 module.exports = UploadMultipleController;
+
