38a796252ba23de4870e168545ba59c2233d72e8
ZhiGangLi <ZhiGangLi@sf-express.com>
2018-08-11 12:03:44 +0800

    * use Object.freeze with nested mode

--- Nodejs/ConstTest/constants.js
+++ Nodejs/ConstTest/constants.js
@@ -1,6 +1,15 @@
 
 module.exports = Object.freeze({
+
 	MY_CONSTANT: 'my-constant-value',
-	ANOTHER_CONSTANT: 'another-constant-value'
+	ANOTHER_CONSTANT: 'another-constant-value',
+
+	BILL_STATUS: Object.freeze({
+		TO_BE_AUDIT: 1,
+		IN_PROGRESS: 2,
+	}),
+
+	MONTH_DELIVERY: 1,
+
 });
 
--- Nodejs/ConstTest/using-constants.js
+++ Nodejs/ConstTest/using-constants.js
@@ -6,3 +6,16 @@ constants.MY_CONSTANT = 'I want to change the value of MY_CONSTANT ...';
 
 console.log(constants.MY_CONSTANT); // 'my-constant-value'
 
+
+console.log(constants.BILL_STATUS.IN_PROGRESS); // 2
+
+constants.BILL_STATUS.IN_PROGRESS = 22;
+
+console.log(constants.BILL_STATUS.IN_PROGRESS); // 2
+
+constants.BILL_STATUS = {};
+
+console.log(constants.BILL_STATUS.IN_PROGRESS); // 2
+
+console.log(constants.MONTH_DELIVERY); // 1
+
