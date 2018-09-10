be010a9b8be1d28bd06c4d0162c5ffa67c77c4c7
ZhiGangLi <ZhiGangLi@sf-express.com>
2018-09-10 11:04:58 +0800

    * testing xlsx with different params

--- Nodejs/XLSXTest/app.js
+++ Nodejs/XLSXTest/app.js
@@ -1,68 +1,34 @@
 const XLSX = require('xlsx');
 
-var workbook = XLSX.readFile('./GearConfig.xlsx');
-var content = workbook.Sheets;
-console.log('content = ', JSON.stringify(content));
+const workbook = XLSX.readFile('./GearConfig.xlsx');
 
-var ret = {worksheets: []};
-
-
-function compareFunc(objA, objB) {
-  var posA = objA.search(/\d+/);
-  var strA = objA.slice(0, posA);
-  var rowA = parseInt(objA.slice(posA));
-
-  var posB = objB.search(/\d+/);
-  var strB = objB.slice(0, posB);
-  var rowB = parseInt(objB.slice(posB));
-
-  if(rowA !== rowB) {
-    return rowA - rowB;
-  } else {
-    if(strA.length !== strB.length) {
-      return strA.length - strB.length;
-    } else {
-      if(strA > strB) {
-        return 1;
-      } else {
-        return -1;
-      }
-    }
-  }
+console.log(`AAAAAAAAA =>\n`);
+for (const sName of workbook.SheetNames) {
+  var wbSheetsInput = XLSX.utils.sheet_to_json(workbook.Sheets[sName]);
+  console.log(`sName = ${sName},\nwbSheetsInput = ${JSON.stringify(wbSheetsInput)}\n`, );
 }
+console.log(`============================================================\n`);
 
 
-for(var sheetName in content) {
-  var obj = {name: sheetName, data: []};
-  var rStr = content[sheetName]['!ref']; 
-  var rList = rStr.split(':');
-  delete content[sheetName]['!ref'];
-  delete content[sheetName]['!margins'];
-
-  var pos = rList[0].search(/\d+/);
-  var rBegin = parseInt(rList[0].slice(pos));
-
-  pos = rList[1].search(/\d+/);
-  var rEnd = parseInt(rList[1].slice(pos));
-
-  for(var i = rBegin; i <= rEnd; i++) {
-    obj.data.push([]);
-  }
-
-  var keysList = Object.keys(content[sheetName]);
-  keysList.sort(compareFunc);
-
-  for(var i = 0; i < keysList.length; i++) {
-    var key = keysList[i];
-    var pos = key.search(/\d+/);
-    var row = parseInt(key.slice(pos));
-
-    var v = content[sheetName][key].v;
-    var o = {'value': v, 'formatCode': 'General'};
-    obj.data[row-1].push(o);
-  }
-
-  ret.worksheets.push(obj);
+console.log(`BBBBBBBBB =>\n`);
+for (const sName of workbook.SheetNames) {
+  var wbSheetsInput = XLSX.utils.sheet_to_json(workbook.Sheets[sName], {header: "A"});
+  console.log(`sName = ${sName},\nwbSheetsInput = ${JSON.stringify(wbSheetsInput)}\n`, );
 }
+console.log(`============================================================\n`);
 
-console.log(`\nret = ${JSON.stringify(ret)}`);
+
+console.log(`CCCCCCCCC =>\n`);
+for (const sName of workbook.SheetNames) {
+  var wbSheetsInput = XLSX.utils.sheet_to_json(workbook.Sheets[sName], {header: 1});
+  console.log(`sName = ${sName},\nwbSheetsInput = ${JSON.stringify(wbSheetsInput)}\n`, );
+}
+console.log(`============================================================\n`);
+
+
+console.log(`DDDDDDDDD =>\n`);
+for (const sName of workbook.SheetNames) {
+  var wbSheetsInput = XLSX.utils.sheet_to_json(workbook.Sheets[sName], {header: 1, raw: false});
+  console.log(`sName = ${sName},\nwbSheetsInput = ${JSON.stringify(wbSheetsInput)}\n`, );
+}
+console.log(`============================================================\n`);
