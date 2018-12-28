20fc6160ea81377ce2834878285c7c4ef3f49d3b
李志刚 <ZhiGangLi@sf-express.com>
2018-12-28 14:18:35 +0800

    * add some more demo code for buffer

--- Nodejs/BufferTest/Backup/app-9.js
+++ Nodejs/BufferTest/Backup/app-9.js
@@ -1,10 +1,10 @@
-var buf = new Buffer(4);
+var buf = new Buffer(6);
 
 let str = '1',
-			offset = 0;
+    offset = 0;
 
 let len = buf.write(str, offset, 1, 'hex');
-console.log(`A: len = ${len}`); // len = 0
+console.log(`A: len = ${len}`); // len = 0  ( However, partially encoded characters will not be written. )
 offset++;
 
 str = '0' + 1;
@@ -13,11 +13,23 @@ console.log(`B: len = ${len}`); // len = 1
 offset++;
 
 str = 0x1.toString();
-buf.write(str, offset, 1, 'hex');
+len = buf.write(str, offset, 1, 'hex');
+console.log(`C: len = ${len}`); // len = 0  ( However, partially encoded characters will not be written. )
+offset++;
+
+str = '0' + 0;
+len = buf.write(str, offset, 1, 'hex');
+console.log(`D: len = ${len}`); // len = 1
+offset++;
+
+str = '0';
+len = buf.write(str, offset, 1, 'hex');
+console.log(`E: len = ${len}`); // len = 0  ( However, partially encoded characters will not be written. )
 offset++;
 
 let n = 1;
-buf.writeUInt8(n, offset);
+len = buf.writeUInt8(n, offset);
+console.log(`F: len = ${len}`); // len = 6  ( len equals 'offset plus the number of bytes written' )
 
-console.log(buf); // <Buffer 00 01 00 01>
+console.log(buf); // <Buffer 00 01 00 00 00 01>
 
