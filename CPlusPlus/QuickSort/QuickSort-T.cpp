9258aff4525243341bedb85b4e8fe70922be3a37
李志刚 <ZhiGangLi@sf-express.com>
2019-02-28 11:20:18 +0800

    + add my code of quick-sort

---
+++ CPlusPlus/QuickSort/QuickSort-T.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+void Swap(int &a, int &b)
+{
+    // a ^= b ^= a ^= b;
+    a ^= b;
+    b ^= a;
+    a ^= b;
+}
+
+int PartitionStd(int lst[], int left, int right)
+{
+    int pivot = lst[left];
+    int j = left;
+    for (int i = left + 1; i <= right; i++)
+    {
+        if (lst[i] < pivot)
+        {
+            j++;
+            if (i != j)
+            {
+                Swap(lst[i], lst[j]);
+            }
+        }
+    }
+    if (j != left)
+    {
+        Swap(lst[j], lst[left]);
+    }
+    return j;
+}
+
+void QuickSort(int lst[], int left, int right)
+{
+    if (left < right)
+    {
+        int idx = PartitionStd(lst, left, right);
+        QuickSort(lst, left, idx - 1);
+        QuickSort(lst, idx + 1, right);
+    }
+}
+
+int main()
+{
+    cout << "QuickSort-T is running ...\n";
+    int lst[] = {12, 45, 9, 748, 12, 56, 3, 89, 4, 48, 2};
+    int len = sizeof(lst) / sizeof(int);
+    QuickSort(lst, 0, len - 1);
+    for (int i = 0; i < len; i++)
+    {
+        cout << " " << lst[i] << " ";
+    }
+    return 0;
+}
