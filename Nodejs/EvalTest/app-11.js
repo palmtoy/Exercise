338bba8bd7955400782550721e0564aea9c89dd9
李志刚 <ZhiGangLi@sf-express.com>
2019-12-04 10:37:41 +0800

    * update the code of app-11.js of 'this'

--- Nodejs/EvalTest/app-11.js
+++ Nodejs/EvalTest/app-11.js
@@ -13,14 +13,31 @@
 */
 
 
+/*
+
+	function foo() {
+		// "use strict";
+		console.log( 'foo: Object.keys(this) = ' + Object.keys(this) );
+		console.log();
+		console.log( `this.a = ${this.a}` );
+	}
+
+	var a = 2;
+	console.log( 'Global: Object.keys(this) = ' +  Object.keys(this) + '\n' );
+
+	foo(); // undefined
+
+*/
+
 function foo() {
-	console.log( 'foo: ' + Object.keys(this) );
-	console.log();
 	console.log( `this.a = ${this.a}` );
 }
 
 var a = 2;
-console.log( 'Global: ' +  Object.keys(this) + '\n' );
 
-foo(); // 2
+( function() {
+	// "use strict";
+  foo(); // undefined
+  }
+) ();
 
