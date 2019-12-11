d6f8ce7677123b7e2d289573aa875c5b4665e1e4
李志刚 <ZhiGangLi@sf-express.com>
2019-12-11 17:09:17 +0800

    + add new testing folder:NodeMediaServerTest

---
+++ Nodejs/NodeMediaServerTest/app.js
@@ -0,0 +1,26 @@
+// $ ffmpeg -re -i ./The-Simpsons-Movie.mp4 -c copy -f flv rtmp://localhost/live/simpsons-stream
+
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+
+};
+
+
+var nms = new NodeMediaServer(config)
+
+nms.run();
+
