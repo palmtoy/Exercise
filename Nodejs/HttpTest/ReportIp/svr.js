3da22ee1b982e6fe9836cb1defc671e43bcfff34
李志刚 <ZhiGangLi@sf-express.com>
2020-01-25 11:06:00 +0800

    * print the cli's real ip(remoteAddress)

--- Nodejs/HttpTest/ReportIp/svr.js
+++ Nodejs/HttpTest/ReportIp/svr.js
@@ -9,16 +9,18 @@ const svrPort = 38086;
 http.createServer(function (req, res) {
 	const queryData = url.parse(req.url, true).query;
 	const now = new Date() + ' ~ ';
+	const cliRealIp = req.connection.remoteAddress;
+	console.log(`${now}Client\'s real IP: ${cliRealIp}`);
 	const tmpData = {'Content-Type': 'text/plain'};
 	if (queryData.ip && queryData.platform) {
 		res.writeHead(200, tmpData);
-		console.log(`${now}Client\'s platform: ${queryData.platform}, Client\'s IP: ${queryData.ip}`);
+		console.log(`${now}Client\'s platform: ${queryData.platform}, Client\'s reported IP: ${queryData.ip}\n`);
 	} else {
 		res.writeHead(400, tmpData);
-		console.log(now + 'Bad Request: QueryData = ' + JSON.stringify(queryData));
+		console.log(`${now}Bad Request: QueryData = ${JSON.stringify(queryData)}\n`);
 	}
 	res.end();
 }).listen(svrPort);
 
-console.log(`Server running at port:${svrPort} ...`);
+console.log(`Server running at port:${svrPort} ...\n`);
 
