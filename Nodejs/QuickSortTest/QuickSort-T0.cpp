14a0e1d18a2ce0d1a2f5fe472f21201b5aaa4e6a
李志刚 <ZhiGangLi@sf-express.com>
2020-05-24 23:11:44 +0800

    + add new testing folder:QuickSortTest with Node.js

---
+++ Nodejs/QuickSortTest/QuickSort-T0.cpp
@@ -0,0 +1,43 @@
+const funcSwap = (lst, i, j) => {
+	const tmp = lst[i];
+	lst[i] = lst[j];
+	lst[j] = tmp;
+}
+
+
+const funcPartition = (lst, left, right) => {
+	let pivot = lst[left];
+	let j = left;
+	for (let i = left + 1; i <= right; i++) {
+		if (lst[i] < pivot) {
+			j++;
+			if (i !== j) {
+				funcSwap(lst, i, j)
+			}
+		}
+	}
+
+	if (j !== left) {
+		funcSwap(lst, left, j)
+	}
+	return j;
+}
+
+
+const quickSort = (lst, left, right) => {
+	if (left >= right) {
+		return;
+	}
+
+	const idx = funcPartition(lst, left, right);
+	quickSort(lst, left, idx - 1);
+	quickSort(lst, idx + 1, right);
+}
+
+
+const lst = [ 2, 4, 6, 8, 0, 9, 7, 5, 3, 1 ];
+
+quickSort(lst, 0, lst.length);
+
+console.log(`lst = ${lst}`);
+
