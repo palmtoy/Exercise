f08fa57598d9f484ebc42e11eea23b21c5ce881d
LiZhigang <lzg2006bj@163.com>
2020-10-06 15:00:46 +0800

    * update testing code files

--- Nodejs/Docker/DockerBuild/app.js
+++ Nodejs/Docker/DockerBuild/app.js
@@ -2,7 +2,8 @@ const http = require('http');
 const os = require('os');
 
 const dbOptions = require('./db-options');
-const knex = require('knex')(dbOptions);
+const knex = require('knex')(dbOptions.mysql);
+const	redisCli = require("redis").createClient(dbOptions.redis);
 
 const hostname = '0.0.0.0';
 const port = 3000;
@@ -11,11 +12,11 @@ const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 
-	let resStr = '';
+	let mysqlStr = 'MySQL ...<br>';
 	knex.from('cars').select("*")
 	.then(dataRows => {
 		for (row of dataRows) {
-			resStr += `${row['id']}: ${row['name']} -> $${row['price']}<br>`;
+			mysqlStr += `${row['id']}: ${row['name']} -> $${row['price']}<br>`;
 		}
 	})
 	.catch(err => {
@@ -24,12 +25,33 @@ const server = http.createServer((req, res) => {
 		console.log(err);
 	})
 	.finally(() => {
-		res.end(`<html><h2>Hi sir, good to see you :-)</h2><h3>${resStr}</h3>From <b>` + os.hostname() + '</b> ~ ' + new Date() + ` ~ Timestamp:${new Date().getTime()}</html>`);
+		let totalRequests = 0;
+		redisCli.incr("httpRequests", function (err, reply) {
+			totalRequests = reply; // stash response in outer scope
+		});
+
+		redisCli.hincrby("ip", req.connection.remoteAddress, 1);
+
+		redisCli.hgetall("ip", function (err, reply) {
+			// This is the last reply, so all of the previous replies must have completed already
+			let redisStr = '<br>Redis ...<br>Total httpRequests: ' + totalRequests + '<br>IP count<br>';
+			Object.keys(reply).forEach(function (ip) {
+				redisStr += '    ' + ip + ': ' + reply[ip] + '<br><br>';
+			});
+			const now = new Date();
+			let resStr = `<html><h2><font color='blue'>Hi sir, good to see you :-)</font></h2>` +
+									 `<font color='navy'>${mysqlStr}</font>` + 
+									 `<font color='red'>${redisStr}</font>` +
+									 `From <font color='fuchsia'><b>${os.hostname()}</b></font>` +
+									 ` ~ ${now} ~ Timestamp: <font color='purple'>${now.getTime()}</font></html>`;
+
+			res.end(resStr);
+		});
 	});
 
 });
 
 server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http:\/\/${hostname}:${port}\/`);
 });
 
