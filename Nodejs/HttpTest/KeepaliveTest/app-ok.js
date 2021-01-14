b62a030a28949d20e1c9fb29e48b6f6912852f88
lizhigang <lizhigang@magictavern.com>
2021-01-14 16:13:47 +0800

    + add new testing folder: HTTP KeepaliveTest

---
+++ Nodejs/HttpTest/KeepaliveTest/app-ok.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const urllib = require('urllib');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	res.end('Hello World, ' + req.connection.remotePort);
+});
+
+server.on('clientError', (err, socket) => {
+	socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+
+server.listen(port);
+
+// Timeout in milliseconds. Default: 5000 (5 seconds)
+server.keepAliveTimeout = 9 * 1000;
+
+//////////////////////////////////////////////////////////////////////////
+
+const keepaliveAgent = new http.Agent({
+	keepAlive: true,
+});
+
+const funcRequest = () => {
+	const reqUrl = 'http://127.0.0.1:' + port;
+	urllib.request(reqUrl, {
+		agent: keepaliveAgent,
+		dataType: 'text',
+	}, (err, data, res) => {
+		if (err) {
+			console.error('Wow ~ error: %s, status: %s', err, res.status, '\n', res);
+			throw err;
+		}
+		console.log('status: %s, data: %s', res.status, data, '\n', res, '\n');
+	});
+}
+
+
+setInterval(funcRequest, 5 * 1000);
+
+funcRequest();
+
