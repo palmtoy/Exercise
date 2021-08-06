05d2bca0ef1770443e497d4460372cb06645cf9c
lizhigang <lizhigang@magictavern.com>
2021-08-06 18:17:11 +0800

    * fork a new worker after a worker exit

--- Nodejs/ClusterTest/Backup/svr-7.js
+++ Nodejs/ClusterTest/Backup/svr-7.js
@@ -1,12 +1,22 @@
 // NODE_DEBUG=cluster node svr.js
 
-var cluster = require('cluster');
+const cluster = require('cluster');
 
 if (cluster.isMaster) {
-  console.log('I am master.');
+
+  console.log(`I am master ${JSON.stringify({process: process.pid})}.`);
+
   cluster.fork();
   cluster.fork();
+
+	cluster.on('exit', (worker, code, signal) => {
+		console.log(`worker #${worker.id} pid:${worker.process.pid} died (${signal || code}). restarting ...`);
+		cluster.fork();
+	});
+
 } else if (cluster.isWorker) {
-  console.log('I am worker #' + cluster.worker.id + '.');
+
+  console.log('I am worker #' + cluster.worker.id + ` ${JSON.stringify({process: process.pid})}` + ', pid:' + cluster.worker.process.pid + '.');
+
 }
 
