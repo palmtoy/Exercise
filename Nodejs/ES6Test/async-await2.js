601849244af2b99f95a80ef3f2417b44bfd2a4c9
lizhigang <lizhigang@magictavern.com>
2021-08-12 17:49:28 +0800

    * call async func with await and without await

--- Nodejs/ES6Test/async-await2.js
+++ Nodejs/ES6Test/async-await2.js
@@ -1,7 +1,17 @@
+
 async function foo() {
 	console.log(1);
-	let a = await 100;
+
+	const a = await 100;
 	console.log(a);
+
+	await new Promise(resolve => {
+		setTimeout(() => {
+			console.log('500');
+			resolve();
+		}, 1000);
+	});
+
 	console.log(2);
 }
 
@@ -9,19 +19,28 @@ console.log(0);
 
 (async () => {
 	await foo();
+
+	console.log();
+
+	foo();
 })();
+
 
 console.log(3);
 
 /*
-
-output:
+output ↓
 
 0
 1
 3
 100
+500
 2
 
+1
+100
+500
+2
 */
 
