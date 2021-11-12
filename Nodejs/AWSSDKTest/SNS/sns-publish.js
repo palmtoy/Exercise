0b47e615817e9d24fe1bcc81c486b1da33d7c738
lizhigang <lizhigang@magictavern.com>
2021-11-12 10:59:57 +0800

    + add aws sdk tools: sns-publish.js for publishing "Raspberry Pi 4B's IP"

---
+++ Nodejs/AWSSDKTest/SNS/sns-publish.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+/*
+	ABOUT THIS NODE.JS SAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
+	https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sns-examples-publishing-messages.html
+*/
+
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+const { networkInterfaces } = require('os');
+
+AWS.config.credentials = new AWS.SharedIniFileCredentials({profile: 'personal-account-long-term'});
+// Set region
+AWS.config.update({region: 'us-west-2'});
+
+const snsObj = new AWS.SNS({apiVersion: '2010-03-31'});
+
+let G_IP_FOR_WIFI = '';
+// const pollingInterval = 30 * 1000; // 30s
+const pollingInterval = 5 * 1000;
+
+function getLocalIp4wifi() {
+	let ip4wifi = '';
+	const nets = networkInterfaces();
+	const results = {};
+
+	for (const name of Object.keys(nets)) {
+		for (const net of nets[name]) {
+			// Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+			if (net.family === 'IPv4' && !net.internal) {
+				results[name] = results[name] || [];
+				results[name].push(net.address);
+			}
+		}
+	}
+
+	if (results.en0 && results.en0.length > 0) {
+		ip4wifi = results.en0[0];
+	}
+	return ip4wifi;
+}
+
+
+async function main() {
+	try {
+		const now = new Date().toString();
+		const tmpIp4wifi = getLocalIp4wifi();
+		if (tmpIp4wifi && G_IP_FOR_WIFI === tmpIp4wifi) {
+			console.log(`Func:main is running ... tmpIp4wifi = ${tmpIp4wifi} ~ ${now}`);
+			return;
+		} else if (!tmpIp4wifi) {
+			G_IP_FOR_WIFI = '';
+			console.log(`Func:main is running, but tmpIp4wifi is empty. ~ ${now}`);
+			return;
+		} else {
+			G_IP_FOR_WIFI = tmpIp4wifi;
+		}
+
+		// Create list topic parameters
+		const params4list = {};
+		let topicArn = '';
+		// Create promise and SNS service object
+		const resObj = await snsObj.listTopics(params4list).promise();
+		console.log(`\n_listTopicsPromise: resObj = ${JSON.stringify(resObj)}\n\n`);
+
+		if (resObj && resObj.Topics && resObj.Topics.length > 0) {
+			topicArn = resObj.Topics[0].TopicArn;
+		}
+		if (!topicArn) {
+			console.error('Can\'t get SNS TopicArn ! Try later ...');
+			return;
+		}
+
+		// Create publish parameters
+		const subject4publish = 'Raspberry Pi 4B\'s IP';
+		const params4publish = {
+			Subject: subject4publish,
+			Message: `${subject4publish}\n\n${tmpIp4wifi}\n\n${now}\n`,
+			TopicArn: topicArn
+		};
+
+		// Create promise and SNS service object
+		const publishTextPromise = snsObj.publish(params4publish).promise();
+
+		// Handle promise's fulfilled/rejected states
+		publishTextPromise.then(
+			function(data) {
+				console.log(`Message ↓ \n\n${params4publish.Message}\nhas been sent to the topic ${params4publish.TopicArn}.\n\n`);
+			}).catch(
+				function(err) {
+					console.error('\n_publishTextPromise:', err, err.stack, '\n');
+				}
+			);
+	} catch(err) {
+		console.error('\n_listTopicsPromise:', err, err.stack, '\n');
+	}
+}
+
+main();
+
+
+setInterval(main, pollingInterval);
+
