143d1ea4f4b7b36e5227a7cd38b8175cab7e02e7
lizhigang <lizhigang@magictavern.com>
2021-12-24 15:10:20 +0800

    + add ant-design-vue and some single file components

--- Nodejs/VueTest/my-project/src/App.vue
+++ Nodejs/VueTest/my-project/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" width="60px">
+    <hello-world msg="Welcome to Your Vue.js App"/>
+    <multi-button/>
+    <br/>
+    <multi-select/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MultiButton from './components/MultiButton.vue'
+import MultiSelect from './components/MultiSelect.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'hello-world': HelloWorld,
+    'multi-button': MultiButton,
+    'multi-select': MultiSelect,
   }
 }
 </script>
---
+++ Nodejs/VueTest/my-project/src/components/MultiSelect.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-select
+    mode="multiple"
+    :default-value="['a1', 'z26']"
+    style="width: 50%"
+    placeholder="Please select"
+    @change="handleChange"
+  >
+    <a-select-option v-for="i in 26" :key="(i + 9).toString(36) + i">
+      {{ (i + 9).toString(36).toUpperCase() + '-' + i }}
+    </a-select-option>
+  </a-select>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleChange(value) {
+      console.log(`selected ${value}`);
+    },
+  },
+};
+</script>
