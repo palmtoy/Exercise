1fa785ebd3a1cb2e99b96f7dbecd7380522d058d
lizhigang <lizhigang@magictavern.com>
2021-12-27 17:37:46 +0800

    * using ref to call child component's method; format the code

--- Nodejs/VueTest/my-vue-project/src/App.vue
+++ Nodejs/VueTest/my-vue-project/src/App.vue
@@ -1,41 +1,63 @@
 <template>
-  <div id='app'>
-    <img alt='Vue logo' src='./assets/logo.png' width='60px'>
-    <hello-world msg='Welcome to My Vue.js App'/>
-    <multi-button @handleBtnHappyClick='handleBtnHappyClick'/>
-    <br/>
-    <multi-select @handleMultiSelectChange='handleMultiSelectChange' />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="60px" />
+    <hello-world msg="Welcome to My Vue.js App" />
+    <multi-button
+      @handleBtnHappyClick="handleBtnHappyClick"
+      @handleBtnPrimaryClick="handleBtnPrimaryClick"
+    />
+    <br />
+    <multi-select
+      ref="multiSelectComp"
+      @handleMultiSelectChange="handleMultiSelectChange"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import MultiButton from './components/MultiButton.vue'
-import MultiSelect from './components/MultiSelect.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import MultiButton from "./components/MultiButton.vue";
+import MultiSelect from "./components/MultiSelect.vue";
 
 export default {
-  data () {
+  data() {
     return {
-      multiSelectItemList: []
-    }
+      multiSelectItemList: [],
+    };
   },
-  name: 'App',
+  name: "App",
   components: {
-    'hello-world': HelloWorld,
-    'multi-button': MultiButton,
-    'multi-select': MultiSelect,
+    "hello-world": HelloWorld,
+    "multi-button": MultiButton,
+    "multi-select": MultiSelect,
   },
   methods: {
     handleMultiSelectChange: function (itemList) {
       this.multiSelectItemList = itemList;
-      console.log(`App.vue ~ _handleMultiSelectChange: itemList = ${JSON.stringify(itemList)}`);
+      console.log(
+        `App.vue ~ _handleMultiSelectChange: itemList = ${JSON.stringify(
+          itemList
+        )}`
+      );
+    },
+    handleBtnPrimaryClick: function () {
+      this.$refs.multiSelectComp.delItemFromSelectedField();
+      console.log(
+        `App.vue ~ _handleBtnPrimaryClick: multiSelectItemList = ${JSON.stringify(
+          this.multiSelectItemList
+        )}`
+      );
     },
     handleBtnHappyClick: function () {
       this.multiSelectItemList.pop();
-      console.log(`App.vue ~ _handleBtnHappyClick: multiSelectItemList = ${JSON.stringify(this.multiSelectItemList)}`);
-    }
-  }
-}
+      console.log(
+        `App.vue ~ _handleBtnHappyClick: multiSelectItemList = ${JSON.stringify(
+          this.multiSelectItemList
+        )}`
+      );
+    },
+  },
+};
 </script>
 
 <style>
--- Nodejs/VueTest/my-vue-project/src/components/MultiSelect.vue
+++ Nodejs/VueTest/my-vue-project/src/components/MultiSelect.vue
@@ -7,7 +7,7 @@
     @change="handleChange"
   >
     <a-select-option v-for="i in 26" :key="(i + 9).toString(36) + i">
-      {{ String.fromCharCode(i + 64) + '-' + i }}
+      {{ String.fromCharCode(i + 64) + "-" + i }}
     </a-select-option>
   </a-select>
 </template>
@@ -16,14 +16,26 @@
 export default {
   data() {
     return {
-      selectedItemList: ['a1', 'b2', 'c3'],
-    }
+      selectedItemList: ["a1", "b2", "c3"],
+    };
   },
   methods: {
     handleChange(itemList) {
       this.selectedItemList = itemList;
-      this.$emit('handleMultiSelectChange', itemList);
+      this.$emit("handleMultiSelectChange", itemList);
     },
+
+    delItemFromSelectedField() {
+      this.selectedItemList.pop();
+      console.log(
+        `MultiSelect.vue ~ _delItemFromSelectedField: selectedItemList = ${JSON.stringify(
+          this.selectedItemList
+        )}`
+      );
+    },
+  },
+  mounted() {
+    this.handleChange(this.selectedItemList);
   },
 };
 </script>
