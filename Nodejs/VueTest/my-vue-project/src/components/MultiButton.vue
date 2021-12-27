da05bccb79a43aafba3dc92cf926f7754df9eb4a
lizhigang <lizhigang@magictavern.com>
2021-12-27 17:49:42 +0800

    * set the space of the buttons in component:MultiButton

--- Nodejs/VueTest/my-vue-project/src/components/MultiButton.vue
+++ Nodejs/VueTest/my-vue-project/src/components/MultiButton.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <a-button type="primary" @click="handlePrimaryClick"> Primary </a-button>
-    <a-button>Default</a-button>
-    <a-button type="danger" @click="handleHappyClick"> Happy :) </a-button>
-    <a-config-provider :auto-insert-space-in-button="false">
-      <a-button type="primary"> Button </a-button>
-    </a-config-provider>
-    <a-button type="link"> Link </a-button>
+    <a-space size='large'>
+      <a-button type="primary" @click="handlePrimaryClick"> Primary </a-button>
+      <a-button>Default</a-button>
+      <a-button type="danger" @click="handleHappyClick"> Happy :) </a-button>
+      <a-config-provider :auto-insert-space-in-button="false">
+        <a-button type="primary"> Button </a-button>
+      </a-config-provider>
+      <a-button type="link"> Link </a-button>
+    </a-space>
   </div>
 </template>
 
