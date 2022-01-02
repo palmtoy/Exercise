c013d21e4f70e87f0b11279237bfa43594dec01b
李志刚 <ZhiGangLi@sf-express.com>
2022-01-02 13:26:18 +0800

    * modify the address of i2c-dev for rpi

--- Nodejs/OledI2CTest/app.js
+++ Nodejs/OledI2CTest/app.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 
-const png2lcd = require('png-to-lcd');
 const i2c = require('i2c-bus');
 const oledI2c = require('oled-i2c-bus');
 
 const opts = {
 	width: 128,
 	height: 64,
-	address: 0x3D
+	address: 0x3C
 };
 
 const i2cBus = i2c.openSync(1);
 const oled = new oledI2c(i2cBus, opts);
 
-png2lcd('./icons/301.png', true, function(err, bitmap) {
-  oled.buffer = bitmap;
-  oled.update();
-});
+oled.clearDisplay();
+oled.drawLine(1, 1, 128, 64, 1);
 
