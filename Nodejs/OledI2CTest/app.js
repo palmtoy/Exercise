aa8d4b439f90a4b613f9826cffd193df2f779552
李志刚 <ZhiGangLi@sf-express.com>
2022-01-02 13:53:00 +0800

    * draw 4 points at 4 corner; draw 4 lines like Britain's national flag

--- Nodejs/OledI2CTest/app.js
+++ Nodejs/OledI2CTest/app.js
@@ -6,12 +6,24 @@ const oledI2c = require('oled-i2c-bus');
 const opts = {
 	width: 128,
 	height: 64,
-	address: 0x3C
+	address: 0x3c
 };
 
 const i2cBus = i2c.openSync(1);
 const oled = new oledI2c(i2cBus, opts);
 
 oled.clearDisplay();
-oled.drawLine(1, 1, 128, 64, 1);
+
+oled.drawPixel([
+	[0, 0, 1],
+	[127, 0, 1],
+	[0, 63, 1],
+	[127, 63, 1],
+]);
+
+oled.drawLine(0, 0, 127, 63, 1);
+oled.drawLine(0, 63, 127, 0, 1);
+
+oled.drawLine(63, 0, 63, 63, 1);
+oled.drawLine(0, 31, 127, 31, 1);
 
