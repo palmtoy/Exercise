4f1fa37683de89ca0f515cdc9665161685a990b4
李志刚 <ZhiGangLi@sf-express.com>
2022-01-15 14:21:59 +0800

    * stop the motor when the board closed

--- Nodejs/JohnnyFiveTest/motor-PCA9685.js
+++ Nodejs/JohnnyFiveTest/motor-PCA9685.js
@@ -8,40 +8,45 @@ const board = new five.Board({
 });
 
 board.on('ready', () => {
-  // Create a new `motor` hardware instance.
-  const motor = new five.Motor({
-    address: 0x60,
+	// Create a new `motor` hardware instance.
+	const motor = new five.Motor({
+		address: 0x60,
 		controller: 'PCA9685',
 		frequency: 1000, // Hz
 		pins: {
 			pwm: 13, // DC-Motor-1, pwm
 			dir: 11, // DC-Motor-1, dir
 		},
-  });
+	});
 
-  // Motor Event API
+	// Motor Event API
 
-  // 'start' events fire when the motor is started.
-  motor.on('start', () => {
-    console.log(`start: ${Date.now()}`);
+	// 'start' events fire when the motor is started.
+	motor.on('start', () => {
+		console.log(`start: ${Date.now()}`);
 		setTimeout(() => {
 			motor.stop();
-		}, 6000);
-  });
+		}, 5000);
+	});
 
-  // 'stop' events fire when the motor is started.
-  motor.on('stop', () => {
-    console.log(` stop: ${Date.now()}`);
+	// 'stop' events fire when the motor is started.
+	motor.on('stop', () => {
+		console.log(` stop: ${Date.now()}`);
 		setTimeout(() => {
 			motor.start();
 		}, 2000);
-  });
+	});
 
-  // Motor API
+	// Motor API
 
-  // start()
-  // Start the motor. `isOn` property set to |true|
+	// start()
+	// Start the motor. `isOn` property set to |true|
 	console.log(new Date().toString());
-  motor.start();
+	motor.start();
+
+	board.on('exit', () => {
+		motor.stop();
+	});
+
 });
 
