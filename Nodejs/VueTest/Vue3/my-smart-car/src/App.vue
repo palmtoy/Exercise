53470137bf4fe1fe01c6872c8e1f2d7291fb9e56
李志刚 <ZhiGangLi@sf-express.com>
2022-01-22 19:00:24 +0800

    + add a new project: my-smart-car

---
+++ Nodejs/VueTest/Vue3/my-smart-car/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/F1.jpg" width="160" height="76" />
+    <CarController msg="Welcome to Car Controller :>" />
+  </div>
+</template>
+
+<script>
+import CarController from "./components/CarController.vue";
+
+export default {
+  name: "App",
+  components: {
+    CarController,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
