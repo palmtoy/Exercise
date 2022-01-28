ccbef5563e4ce2388d953730b4730ab7e5ac08d8
lizhigang <lizhigang@magictavern.com>
2022-01-28 19:41:50 +0800

    + add testing code of MicroWebSrv2 with Micropython on ESP8266

---
+++ Python/Py/micro-web-srv/main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+from MicroWebSrv2  import *
+from time          import sleep
+
+# ============================================================================
+
+@WebRoute(GET, '/test-redir')
+def RequestTestRedirect(microWebSrv2, request) :
+    request.Response.ReturnRedirect('/test.pdf')
+
+# ============================================================================
+
+@WebRoute(GET, '/test-post', name='TestPost1/2')
+def RequestTestPost(microWebSrv2, request) :
+    content = """\
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>POST 1/2</title>
+        </head>
+        <body>
+            <h2>MicroWebSrv2 - POST 1/2</h2>
+            User address: %s<br />
+            <form action="/test-post" method="post">
+                First name: <input type="text" name="firstname"><br />
+                Last name:  <input type="text" name="lastname"><br />
+                <input type="submit" value="OK">
+            </form>
+        </body>
+    </html>
+    """ % request.UserAddress[0]
+    request.Response.ReturnOk(content)
+
+# ------------------------------------------------------------------------
+
+@WebRoute(POST, '/test-post', name='TestPost2/2')
+def RequestTestPost(microWebSrv2, request) :
+    data = request.GetPostedURLEncodedForm()
+    try :
+        firstname = data['firstname']
+        lastname  = data['lastname']
+    except :
+        request.Response.ReturnBadRequest()
+        return
+    content   = """\
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>POST 2/2</title>
+        </head>
+        <body>
+            <h2>MicroWebSrv2 - POST 2/2</h2>
+            Hello %s %s :)<br />
+        </body>
+    </html>
+    """ % ( MicroWebSrv2.HTMLEscape(firstname),
+            MicroWebSrv2.HTMLEscape(lastname) )
+    request.Response.ReturnOk(content)
+
+# ============================================================================
+
+print()
+
+# Loads the PyhtmlTemplate module globally and configure it,
+pyhtmlMod = MicroWebSrv2.LoadModule('PyhtmlTemplate')
+pyhtmlMod.ShowDebug = True
+pyhtmlMod.SetGlobalVar('TestVar', 12345)
+
+# Instanciates the MicroWebSrv2 class,
+mws2 = MicroWebSrv2()
+
+# For embedded MicroPython, use a very light configuration,
+mws2.SetEmbeddedConfig()
+
+# Starts the server as easily as possible in managed mode,
+mws2.StartManaged()
+
+# Main program loop until keyboard interrupt,
+try :
+    while mws2.IsRunning :
+        sleep(1)
+except KeyboardInterrupt :
+    pass
+
+# End,
+print()
+mws2.Stop()
+print('Bye')
+print()
+
+# ============================================================================
+
