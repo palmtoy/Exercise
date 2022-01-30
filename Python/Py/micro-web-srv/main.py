5929410d4d7013de4af74edc10871150a83e0f42
李志刚 <ZhiGangLi@sf-express.com>
2022-01-30 09:59:10 +0800

    * simplify the code of micro-web-srv on ESP8266

--- Python/Py/micro-web-srv/main.py
+++ Python/Py/micro-web-srv/main.py
@@ -12,12 +12,9 @@ ledLight = Pin(2, Pin.OUT)
 led_state = "OFF"
 
 def web_page():
-    html = """<html>
-
+    html = """
+<html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <style>
         html {
             font-family: Arial;
@@ -25,39 +22,41 @@ def web_page():
             margin: 0px auto;
             text-align: center;
         }
-
-        .button {
-            background-color: #ce1b0e;
-            border: none;
+        .buttonOn {
+            background-color: red;
             color: white;
-            padding: 16px 40px;
+            padding: 80px 200px;
             text-align: center;
-            text-decoration: none;
             display: inline-block;
-            font-size: 16px;
+            font-size: 80px;
             margin: 4px 2px;
             cursor: pointer;
         }
-
-        .button1 {
-            background-color: #000000;
+        .buttonOff {
+            background-color: black;
+            color: white;
+            padding: 80px 180px;
+            text-align: center;
+            display: inline-block;
+            font-size: 80px;
+            margin: 4px 2px;
+            cursor: pointer;
         }
     </style>
 </head>
 
 <body>
-    <h2>ESP MicroPython Web Server</h2>
-    <p>LED state: <strong>""" + led_state + """</strong></p>
+    <br/>
+    <br/>
+    <h1>ESP MicroPython Web Server</h1>
+    <h2>LED state: <strong>""" + led_state + """</strong></h2>
     <p>
-        <i class="fas fa-lightbulb fa-3x" style="color:#c81919;"></i>
-        <a href=\"?led_2_on\"><button class="button">LED ON</button></a>
+        <a href=\"?led_2_on\"><button class="buttonOn">LED ON</button></a>
     </p>
     <p>
-        <i class="far fa-lightbulb fa-3x" style="color:#000000;"></i>
-        <a href=\"?led_2_off\"><button class="button button1">LED OFF</button></a>
+        <a href=\"?led_2_off\"><button class="buttonOff">LED OFF</button></a>
     </p>
 </body>
-
 </html>"""
     return html
 
