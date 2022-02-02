b060367cd6fd691a0fdc4f5cf1fc8140c53471c1
李志刚 <ZhiGangLi@sf-express.com>
2022-02-02 21:00:44 +0800

    * get the input char and let the dc motor do as the cmd

--- Python/Py/raspberry-pi/dc-motor/h450-dc-motor.py
+++ Python/Py/raspberry-pi/dc-motor/h450-dc-motor.py
@@ -2,23 +2,34 @@
 # REF: https://www.electronicshub.org
 
 from dcmotor import DCMotor
-from time import sleep 
 
 dcMotorA = DCMotor(9, 10)  # GPIO9, GPIO10
 
+def printCmdTips():
+    print('\nPlease enter the following cmd to continue ↓')
+    print('f: Run forward, b: Run backward, l: low speed, m: medium speed, h: high speed, s: stop\n')
+
 def go():
-    dcMotorA.runForward()
-    sleep(3)
-    dcMotorA.setMediumSpeed()
-    sleep(3)
-    dcMotorA.stop()
-    sleep(1)
-    dcMotorA.runBackward()
-    sleep(3)
-    dcMotorA.setHighSpeed()
-    sleep(3)
-    dcMotorA.setLowSpeed()
-    sleep(3)
+    printCmdTips()
+    while True:
+        x = input()
+        try:
+            if x == 'f':
+                dcMotorA.runForward()
+            elif x == 'b':
+                dcMotorA.runBackward()
+            elif x == 's':
+                dcMotorA.stop()
+            elif x == 'l':
+                dcMotorA.setLowSpeed()
+            elif x == 'm':
+                dcMotorA.setMediumSpeed()
+            elif x == 'h':
+                dcMotorA.setHighSpeed()
+            else:
+                printCmdTips()
+        except Exception as e:
+            print('Error:', e)
 
 if __name__ == "__main__":
     try:
