769ef7b5c4913f596251db1fc6db7b753cff0e4d
李志刚 <ZhiGangLi@sf-express.com>
2022-02-06 16:36:27 +0800

    * adjust some comments of the code file:supersonic-distance with the device:HC-SR04

--- Python/Py/raspberry-pi/supersonic-distance.py
+++ Python/Py/raspberry-pi/supersonic-distance.py
@@ -11,8 +11,8 @@ pinLedRed = 18 # GPIO18
 
 # Use GPIO numbers not pin numbers [ BCM : Broadcom SOC ( System on Chip ) Channel ]
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pinTrig, GPIO.OUT)  # 超声波发送引脚, 高电平时发出 40K Hz 的超声波
-GPIO.setup(pinEcho, GPIO.IN)  # 超声波接收检测引脚, 当接收到返回的超声波时输出高电平
+GPIO.setup(pinTrig, GPIO.OUT)  # 对应触发超声波发射的引脚, 高电平时发射 8个 40K Hz的超声波; pinEcho 输出高电平 ( GPIO.HIGH )
+GPIO.setup(pinEcho, GPIO.IN)  # 对应超声波接收检测的引脚, 当接收到返回的超声波时输出低电平 ( GPIO.LOW )
 
 GPIO.setup(pinLedRed, GPIO.OUT)
 
@@ -21,7 +21,7 @@ GPIO.setup(pinLedRed, GPIO.OUT)
 def sendTriggerPulse():
   # 发射超声波
   GPIO.output(pinTrig, GPIO.HIGH)
-  # 保持 15us 的超声波发射, 避免能量太低无法返回
+  # 给 pinTrig 输入一个长为 15us 的高电平方波来触发超声波发射
   time.sleep(0.000015)
   # 停止发射超声波
   GPIO.output(pinTrig, GPIO.LOW)
@@ -31,20 +31,21 @@ def waitForEcho(value, timeoutCnt):
   # 持续获取 Echo 的状态
   while GPIO.input(pinEcho) != value and timeoutCnt > 0:
     timeoutCnt -= 1
+    # if value == GPIO.HIGH:
+    #   print('timeoutCnt = %d' % timeoutCnt)
     pass
 
 
 # calc distance
 def calcDistance():
   echoTimeoutCnt = 10000
-  # 发射
+  # 发射超声波, pinEcho 输出高电平
   sendTriggerPulse()
-  # 尝试接收超声波 ( 高电平 )
   waitForEcho(GPIO.HIGH, echoTimeoutCnt)
-  startTime = time.time()  # 发现高电平 (即: 接收超声波的那一刻) 时开时计时
-  # 尝试接收低电平
+  startTime = time.time()  # pinEcho 输出高电平 (即: pinTrig 发出超声波的那一刻) 时开时计时
+  # 接收到返回的超声波, pinEcho 输出低电平
   waitForEcho(GPIO.LOW, echoTimeoutCnt)
-  finishTime = time.time()  # 发现低电平 (即: 不再能接收超声波的那一刻) 时停止计时
+  finishTime = time.time()  # pinEcho 输出低电平 (即: pinEcho 接收超声波的那一刻) 时停止计时
   echoInterval = finishTime - startTime
   cmDistance = echoInterval * 340 * 100 / 2  # unit: cm; 声速: 340m/s
   print('startTime = %f, finishTime = %f, echoInterval = %f, cmDistance = %.1f(cm)' % (startTime, finishTime, echoInterval, cmDistance))
@@ -53,6 +54,7 @@ def calcDistance():
 
 def cleanup():
   GPIO.cleanup()
+  print('\n', time.asctime( time.localtime(time.time()) ), '\nBye ~')
 
 
 def run():
@@ -65,7 +67,7 @@ def run():
         GPIO.output(pinLedRed, GPIO.LOW)  # 灭
       time.sleep(0.5)
   except KeyboardInterrupt:
-      print('\n', time.asctime( time.localtime(time.time()) ), '\nBye ~')
+      pass
 
 
 if __name__ == "__main__":
