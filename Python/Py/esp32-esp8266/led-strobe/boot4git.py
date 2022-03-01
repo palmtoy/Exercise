a1d156cbecb09f9e547075b164fac7202066f6eb
李志刚 <ZhiGangLi@sf-express.com>
2022-03-01 22:41:52 +0800

    + add a new template code file:boot4git.py

---
+++ Python/Py/esp32-esp8266/led-strobe/boot4git.py
@@ -0,0 +1,17 @@
+import network
+import esp
+import time
+
+esp.osdebug(None)
+
+wlanObj = network.WLAN(network.STA_IF)
+wlanObj.active(True)
+wlanObj.config(dhcp_hostname = 'drone')
+wlanObj.connect('TP2.4G_***_ssid', 'TP2.4G_***_pwd')
+
+while wlanObj.isconnected() == False:
+  time.sleep(0.1)
+  pass
+
+print('DHCP_HOSTNAME: {}.local'.format(wlanObj.config('dhcp_hostname')))
+print('Connection successful:', wlanObj.ifconfig())
