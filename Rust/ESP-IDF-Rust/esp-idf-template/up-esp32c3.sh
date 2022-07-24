4d9a635c4ed1c6b291f84e38058493003909be2c
LiZhigang <lzg2006bj@163.com>
2022-07-24 14:41:09 +0800

    * update the up-esp32c3.sh to use the new image:idf-rust:esp32c3_classic

--- Rust/ESP-IDF-Rust/esp-idf-template/up-esp32c3.sh
+++ Rust/ESP-IDF-Rust/esp-idf-template/up-esp32c3.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker run --name esp32c3-compiler --rm -v $HOME/Workspace/GitHub/Exercise/Rust/ESP-IDF-Rust/esp-idf-template:/esp-idf-template -w /esp-idf-template -it espressif/idf-rust:esp32c3_v4.4_1.62.0.0
+docker run --name esp32c3-compiler_original --rm \
+	-v $HOME/Repository/Software/Programming/IDF-Rust/registry:/home/esp/.cargo/registry \
+	-v $HOME/Workspace/GitHub/Exercise/Rust/ESP-IDF-Rust/esp-idf-template:/esp-idf-template \
+	-w /esp-idf-template \
+	-it espressif/idf-rust:esp32c3_v4.4_1.62.0.0_classic
 
