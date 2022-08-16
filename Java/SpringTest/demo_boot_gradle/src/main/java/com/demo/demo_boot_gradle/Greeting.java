3e4fb8117cdc5f39dafcc517fda45ad24d1d5853
lizhigang <lizhigang@magictavern.com>
2022-08-16 17:35:02 +0800

    * using lombok annotation in class:Greeting

--- Java/SpringTest/demo_boot_gradle/src/main/java/com/demo/demo_boot_gradle/Greeting.java
+++ Java/SpringTest/demo_boot_gradle/src/main/java/com/demo/demo_boot_gradle/Greeting.java
@@ -1,22 +1,12 @@
 package com.demo.demo_boot_gradle;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Greeting {
 	private String message;
-
-	public Greeting() {
-	}
-
-	public Greeting(String message) {
-		this.message = message;
-	}
-
-	public String getMessage() {
-		return this.message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
 
 	@Override
 	public String toString() {
