5530438c9cea4a9b6acc5ac7114ec14af534fa09
lizhigang <lizhigang@magictavern.com>
2022-08-16 16:04:04 +0800

    + add unit test code file

---
+++ Java/SpringTest/demo_boot_gradle/src/test/java/com/demo/demo_boot_gradle/DemoBootGradleApplicationTests.java
@@ -0,0 +1,36 @@
+package com.demo.demo_boot_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+// We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoBootGradleApplicationTests {
+
+	// Spring Boot will create a `WebTestClient` for you,
+	// already configure and ready to issue requests against "localhost:RANDOM_PORT"
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	public void testDemoHello() {
+		webTestClient
+				// Create a GET request to test an endpoint
+				.get().uri("/hello")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				// and use the dedicated DSL to test assertions against the response
+				.expectStatus().isOk()
+				.expectBody(Greeting.class).value(greeting -> {
+					assertThat(greeting.getMessage()).isEqualTo("Hello, Spring!");
+				});
+	}
+}
