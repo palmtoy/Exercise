a6bac84918373550a1617f047ddf8c1969522721
lizhigang <lizhigang@magictavern.com>
2022-08-20 17:39:11 +0800

    * print some logs when receiving a http request

--- Java/SpringTest/demo_boot_gradle/src/main/java/com/demo/demo_boot_gradle/CourseHandler.java
+++ Java/SpringTest/demo_boot_gradle/src/main/java/com/demo/demo_boot_gradle/CourseHandler.java
@@ -18,13 +18,17 @@ public class CourseHandler {
 
   public Mono<ServerResponse> getCourseById(ServerRequest request) {
     Integer id = Integer.parseInt(request.pathVariable("id"));
+    System.out.println("Receive a new request /courses/" + id);
     return ServerResponse.ok().contentType(MediaType.APPLICATION_OCTET_STREAM)
         .body(BodyInserters.fromValue(courseRepo.getCourseById(id)));
   }
 
   public Mono<ServerResponse> putNewCourse(ServerRequest request) {
     return request.bodyToMono(Course.class)
-        .flatMap(msg -> Mono.just(courseRepo.putNewCourse(msg)))
+        .flatMap(msg -> {
+          System.out.println("Receive a new request /courses/save, msg => " + msg);
+          return Mono.just(courseRepo.putNewCourse(msg));
+        })
         .flatMap(id -> ServerResponse.ok().bodyValue(courseRepo.getCourseById(id)));
   }
 
