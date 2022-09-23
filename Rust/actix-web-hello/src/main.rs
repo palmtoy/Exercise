209ffce7df90c242c3026003be17747f17adfad9
lizhigang <lizhigang@magictavern.com>
2022-09-23 16:42:12 +0800

    * send HTTP POST request with JSON body to httpbin

--- Rust/actix-web-hello/src/main.rs
+++ Rust/actix-web-hello/src/main.rs
@@ -7,6 +7,7 @@ use std::collections::HashMap;
 #[get("/")]
 async fn hello() -> impl Responder {
     let _req_ret = req_httpbin().await;
+    println!("fn_hello: _req_ret = {:#?}", _req_ret);
     let now = get_cur_time();
     HttpResponse::Ok().body(format!("\n{} ~ {}\n\n", now, "Hello World!"))
 }
@@ -16,15 +17,52 @@ async fn req_httpbin() -> Result<(), Box<dyn std::error::Error>> {
         .await?
         .json::<HashMap<String, String>>()
         .await?;
-    println!("_req_httpbin: resp = {:#?}", resp);
+    println!("fn_req_httpbin: resp = {:#?}", resp);
     Ok(())
 }
 
 // curl -v http://localhost:8080/echo -d '{"foo": "bar"}'
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
+    let _post_ret = post_httpbin().await;
+    println!("\nfn_echo: _post_ret.is_ok = {}", _post_ret.is_ok());
+    println!("fn_echo: _post_ret = {:#?}", _post_ret);
     let now = get_cur_time();
     HttpResponse::Ok().body(format!("\n{} ~ {}\n\n", now, req_body))
+}
+
+async fn post_httpbin() -> Result<(), Box<dyn std::error::Error>> {
+    // This will POST a body of `{"lang": "rust"}`
+    let mut map = HashMap::new();
+    map.insert("lang", "rust");
+
+    let client = reqwest::Client::new();
+    let resp = client
+        .post("http://httpbin.org/post")
+        .json(&map)
+        .send()
+        .await?;
+    println!("\nfn_post_httpbin: resp = {:#?}", resp);
+    let buf = resp.bytes().await?;
+    println!("fn_post_httpbin: buf = {:#?}", buf);
+    let str_body = match String::from_utf8(buf.to_vec()) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    let json_body = match json::parse(str_body.as_str()) {
+        Ok(jv) => jv,
+        Err(_) => {
+            let err_msg = "\nfn_post_httpbin: Failed to parse the HTTP body to JSON!\n";
+            println!("{}", err_msg);
+            json::JsonValue::Null
+        }
+    };
+    if json_body != json::JsonValue::Null {
+        println!("\nfn_post_httpbin: json_body.data = {:#?}\n", json_body["data"]);
+    } else {
+        println!("\nfn_post_httpbin: json_body is Null\n");
+    }
+    Ok(())
 }
 
 // curl -v http://localhost:8080/hey
