d0fa664a176ae18857c41780346043a18c1e3e30
lizhigang <lizhigang@magictavern.com>
2023-05-08 16:19:43 +0800

    + add a testing folder: NunjucksTest

---
+++ Nodejs/NunjucksTest/app.js
@@ -0,0 +1,27 @@
+const nunjucks = require('nunjucks');
+
+const strTemplate1 = `
+~ 脚本: {{ scriptsName }}
+~ 应用: {{ appName }}
+~ 平台: {{ platform }}
+~ 包名: {{ package }}
+`;
+
+const dataObj = {
+	scriptsName: 'Smoke Test',
+	appName: 'SKY',
+	platform: 'iOS',
+	package: 'com.sky.blue',
+};
+
+const str1 = nunjucks.renderString(strTemplate1, dataObj);
+console.log(`nunjucks str(1) ↓\n ${str1}`);
+
+const strTemplate2 = `
+~ 脚本: {{ scriptsName }}
+~ 应用: {{ appName }}
+~ 包名: {{ package }}
+`;
+const str2 = nunjucks.renderString(strTemplate2, dataObj);
+console.log(`\n\nnunjucks str(2) ↓\n ${str2}`);
+
