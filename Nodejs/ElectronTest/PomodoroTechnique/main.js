32d7ff73896fed7f7c137440d8590abeae467d27
lizhigang <lizhigang@magictavern.com>
2024-04-11 20:19:45 +0800

    + add a new testing folder -- PomodoroTechnique

---
+++ Nodejs/ElectronTest/PomodoroTechnique/main.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow, ipcMain, Notification } = require('electron');
+
+let mainWindow = null;
+
+function handleIPC() {
+  ipcMain.handle('notification', async (e, { body, title, actions, closeButtonText }) => {
+    let res = await new Promise((resolve, reject) => {
+      console.log({
+        title,
+        body,
+        actions,
+        closeButtonText,
+      });
+      let notification = new Notification({
+        title,
+        body,
+        actions,
+        closeButtonText,
+      });
+      notification.show();
+      notification.on('action', function (event) {
+        resolve({ event: 'action' });
+      });
+      notification.on('close', function (event) {
+        resolve({ event: 'close' });
+      });
+    });
+    return res;
+  });
+}
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 250,
+    height: 350,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.loadFile('./index.html');
+
+  return mainWindow;
+}
+
+app.whenReady().then(() => {
+  handleIPC();
+  createMainWindow();
+});
