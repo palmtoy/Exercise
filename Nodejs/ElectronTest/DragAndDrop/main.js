cb8be92d95793942ff05b2e0c36e92019faac289
lizhigang <lizhigang@magictavern.com>
2024-06-12 19:11:26 +0800

    + add drag-and-drop-3.txt

--- Nodejs/ElectronTest/DragAndDrop/main.js
+++ Nodejs/ElectronTest/DragAndDrop/main.js
@@ -21,6 +21,7 @@ const icon = fs.createWriteStream(iconName);
 // Create a new file to copy - you can also copy existing files.
 fs.writeFileSync(path.join(__dirname, 'drag-and-drop-1.md'), '# First file to test drag and drop');
 fs.writeFileSync(path.join(__dirname, 'drag-and-drop-2.md'), '# Second file to test drag and drop');
+fs.writeFileSync(path.join(__dirname, 'drag-and-drop-3.txt'), '# Third file to test drag and drop ~ 3');
 
 https.get('https://img.icons8.com/ios/452/drag-and-drop.png', response => {
   response.pipe(icon);
--- Nodejs/ElectronTest/DragAndDrop/renderer.js
+++ Nodejs/ElectronTest/DragAndDrop/renderer.js
@@ -7,3 +7,8 @@ document.getElementById('drag2').ondragstart = event => {
   event.preventDefault();
   window.electron.startDrag('drag-and-drop-2.md');
 };
+
+document.getElementById('drag3').ondragstart = event => {
+  event.preventDefault();
+  window.electron.startDrag('drag-and-drop-3.txt');
+};
