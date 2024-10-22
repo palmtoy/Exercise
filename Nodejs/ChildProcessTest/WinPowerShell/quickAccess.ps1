75247be5695992b966d3913a5f983d9b5583bc20
lizhigang <lizhigang@magictavern.com>
2024-10-22 12:08:53 +0800

    * take ', ' as the separator of folders' path

--- Nodejs/ChildProcessTest/WinPowerShell/quickAccess.ps1
+++ Nodejs/ChildProcessTest/WinPowerShell/quickAccess.ps1
@@ -1,7 +1,13 @@
 # 打印出所有快速访问的文件&文件夹的绝对路径
 $quickAccessFilesAndFolders = New-Object -ComObject shell.application
 $quickAccessNamespace = $quickAccessFilesAndFolders.Namespace('shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}')
+$bPrintFlag = $false
 $quickAccessNamespace.Items() | ForEach-Object {
-  $_.Path
+  $bPrintFlag = $true
+  $str = $_.Path + ', '
+  Write-Host $str -NoNewline
+}
+if ($bPrintFlag) {
+  Write-Host ''
 }
 
